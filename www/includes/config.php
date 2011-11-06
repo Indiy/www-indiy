6da37bd860e55b6a495a85fc4f1dea44b20776e5
Jim Lake <jim@blueskylabs.com>
2011-11-06 13:54:09 -0800

    updates to always be able to find server_config.php

--- www/includes/config.php
+++ www/includes/config.php
@@ -1,18 +1,17 @@
 <?php
+    $new_include_path = get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . "../..";
+    set_include_path($new_include_path);
 
     // This file should have db configuration
     require_once("server_config.php");
-
 	session_start();
 
-	$prefix 			= "mydna_";
-	
-	$jibya = mysql_fetch_array(mysql_query("select * from `{$prefix}musicplayer_config` where `id`='1' limit 1"));
-	
-	$domainName 		= $jibya["domain"];
-	$trueSiteUrl 		= $jibya["url"];
-	$siteUrl 			= $jibya["url"];
-	$playerUrl 			= $jibya["playerUrl"];
-	$siteTitle 			= $jibya["title"];
+	$prefix = "mydna_";
 
+	$jibya = mysql_fetch_array(mysql_query("select * from `{$prefix}musicplayer_config` where `id`='1' limit 1"));
+	$domainName = $jibya["domain"];
+	$trueSiteUrl = $jibya["url"];
+	$siteUrl = $jibya["url"];
+	$playerUrl = $jibya["playerUrl"];
+	$siteTitle = $jibya["title"];
 ?>
