5c442b6c7a950480db7ea8a80af7fd9482cf9bf5
Jim Lake <jim@blueskylabs.com>
2011-11-07 14:02:11 -0800

    updated add user form

--- www/manage/add_user.php
+++ www/manage/add_user.php
@@ -8,7 +8,7 @@
 		exit();
 	}
 	
-	if ($_POST['submit'] != "") 
+	if($_POST['artist'] != "") 
     {
         $artist = $_POST['artist'];
         $url = $_POST['url'];
@@ -25,31 +25,63 @@
 		exit();
 	}
 ?>
-				
+<script type="text/javascript">
+
+function onSubmit()
+{
+    $('#add_user_submit').hide();
+    $('#status').text("Adding user...");
+    var artist = $('#artist').val();
+    var url = $('#url').val();
+    var email = $('#email').val();
+    var password = $('#password').val();
+    
+    var post_url = "?artist=" + escape(artist);
+    post_url += "&url=" + escape(url);
+    post_url += "&email=" + escape(email);
+    post_url += "&password=" + escape(password);
+    jQuery.ajax(
+    {
+        type: 'POST',
+        url: post_url,
+        dataType: 'json',
+        success: function(data) 
+        {
+            $('#status').text("User Added");
+        },
+        error: function()
+        {
+            $('#status').text("User Add Failed!");
+        }
+    });
+}
+</script>
+
 <div id="popup">
     <div class="addcontent">
         <h2 class="title"  id="demonstrations">Add Artist</h2>
-        <form id="ajax_from" method="post" enctype="multipart/form-data" action="add_user.php">
+        <form id="none" method="post" enctype="multipart/form-data" action="add_user.php">
             <div id="form_field">
             <div class="clear"></div>
             
             <label>Artist</label>
-            <input type="text" name="artist" value="" class="text" />
+            <input id='artist' type="text" name="artist" value="" class="text" />
             <div class="clear"></div>
             
             <label>URL</label>
-            <input type="text" name="url" value="" class="text" />
+            <input id='url' type="text" name="url" value="" class="text" />
             <div class="clear"></div>
 
             <label>Email</label>
-            <input type="text" name="email" value="" class="text" />
+            <input id='email' type="text" name="email" value="" class="text" />
             <div class="clear"></div>
             
             <label>Password</label>
-            <input type="password" name="password" value="" class="text" />
+            <input id='password' type="password" name="password" value="" class="text" />
             <div class="clear"></div>
             
-            <input type="submit" name="submit" value="submit" class="submit" />
+            <button id='add_user_submit' class="submit" onclick='onSubmit();'>Submit</button>
+            <div id='status'></div>
         </div>
         </form>
     </div>
