fd8169e09d6e0de136de45620a934e21f7495618
Jim Lake <jim@blueskylabs.com>
2011-11-06 21:45:37 -0800

    maybe working add user

--- www/manage/add_user.php
+++ www/manage/add_user.php
@@ -4,82 +4,31 @@
 	require_once '../includes/functions.php';
 	if($_SESSION['sess_userId']=="")
 	{
-		header("location: index.php");
+		header("Location: index.php");
 		exit();
 	}
 	
-	$database = "[p]musicplayer_content";
-    $_SESSION['tabOpen']='pages';
-	if ($_POST["submit"] != "") {
-		
-		if ($_POST["id"] != "") {
-			$row = mf(mq("select `id`,`image` from `{$database}` where `id`='{$_POST["id"]}'"));
-			$old_logo = $row["image"];
-		}
-		
-		$content_name = my($_POST["name"]);
-		$content_video = $_POST["video"];
-		$content_body = my($_POST["body"]);
-		
-		// Upload Image
-		if(!empty($_FILES["logo"]["name"])){
-			if (is_uploaded_file($_FILES["logo"]["tmp_name"])) {
-				$content_logo = $artistid."_".strtolower(rand(11111,99999)."_".basename(cleanup($_FILES["logo"]["name"])));
-				@move_uploaded_file($_FILES['logo']['tmp_name'], '../artists/images/' . $content_logo);
-			} else {
-				if ($old_logo != "") {
-					$content_logo = $old_logo;
-				}
-			}
-		}else{
-					$content_logo = $old_logo;
-		}
-		
-		$tables = "artistid|name|image|video|body";
-		$values = "{$artistid}|{$content_name}|{$content_logo}|{$content_video}|{$content_body}";
-		
-		if ($_POST["id"] != "") {
-			update($database,$tables,$values,"id",$_POST["id"]);
-		} else {
-			insert($database,$tables,$values);
-		}
-		
-		$postedValues['imageSource'] = "../artists/images/".$content_logo;
-		//$postedValues['video_sound'] = "artists/video/".$audio_sound;
-		$postedValues['success'] = "1";
-		$postedValues['postedValues'] = $_REQUEST;
-		//echo '{"Name":"'.$audio_name.'","imageSource":"artists/images/'.$audio_logo.'","":"","audio_sound":"artists/audio/'.$audio_sound.'","success":1}';
-		echo json_encode($postedValues);
-		exit;
-	}
-	
-	if ($_GET["id"] != "") {
-		$artistid=$_REQUEST['artist_id'];
-		$row = mf(mq("select * from `{$database}` where `id`='{$_GET["id"]}' and `artistid`='{$artistid}'"));
-		$content_id = $row["id"];
-		$content_name = $row["name"];
-		$content_logo = $row["image"];
-		$content_video = $row["video"];
-		$content_body = $row["body"];
-		$head_title	=	"Edit";
-	}else{
-		$head_title	=	"Add";
-	}
+	if ($_POST['submit'] != "") 
+    {
+        $artist = $_POST['artist'];
+        $url = $_POST['url'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-	
-	if ($content_logo != "") {
-		$content_logo = '<img src="../artists/images/'.$content_logo.'" style="margin-top: 5px; height: 25px;" />';
+        $tables = "artist|url|email|password";
+		$values = "{$artist}|{$url}|{$email}|{$password}";
+        insert('[p]musicplayer',$tables,$values);
+
+        $postedValues['success'] = "1";
+		$postedValues['postedValues'] = $_REQUEST;
+		echo json_encode($postedValues);
+		exit();
 	}
-	
-	$content_name = stripslashes($content_name);
-	$content_body = stripslashes($content_body);
 ?>
 				
-				
 <div id="popup">
-    <?=$successMessage;?>
     <div class="addcontent">
-        <h2 class="title"  id="demonstrations"><?=$head_title?> Page</h2>
+        <h2 class="title"  id="demonstrations">Add Artist</h2>
         <form id="ajax_from" method="post" enctype="multipart/form-data" action="add_user.php">
             <div id="form_field">
             <div class="clear"></div>
