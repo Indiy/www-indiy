99ca0199bff71da17bdffd5210c98885333b8413
Alphonso Morris <amorris@levasent.com>
2011-11-09 08:30:20 -0500

    Updated links.

--- www/manage/footer.php
+++ www/manage/footer.php
@@ -4,50 +4,53 @@
     <aside>
     <h5>ARTIST</h5>
     <ul>
-    <li><a href="#">All</a></li>
-    <li><a href="#">Rock</a></li>
-    <li><a href="#">Hip-Hop</a></li>
-    <li><a href="#">Dance</a></li>
-    <li><a href="#">Country</a></li>
+    <li><a href="artists.php">All</a></li>
+    <li><a href="music.php">Music</a></li>
+    <li><a href="art.php">Art</a></li>
+  
     </ul>
     </aside>
 
     <aside class="benefits">
     <h5>BENEFITS</h5>
     <ul>
-    <li><a href="#">Be Heard</a></li>
-    <li><a href="#">Be Seen</a></li>
-    <li><a href="#">Be Independent</a></li>
+     <li><a href="be-heard.html">Be Heard</a></li>
+     <li><a href="be-seen.html">Be Seen</a></li>
+     <li><a href="be-independent.html">Be Independent</a></li>
     </ul>
     </aside>
 
     <aside>
     <h5>SUPPORT</h5>
     <ul>
-    <li><a href="#">FAQs</a></li>
-    <li><a href="#">Help</a></li>
+    <li><a href="faq.html">FAQs</a></li>
+    <li><a href="tour.php">Help</a></li>
+    <li><a href="privacy.html">Privacy</a></li>
+    <li><a href="terms_service.html">Terms</a></li>
     </ul>
     </aside>
 
     <aside>
-    <h5>LEGAL</h5>
+    <h5>AFFILIATES</h5>
     <ul>
-    <li><a href="#">Privacy</a></li>
-    <li><a href="#">Terms</a></li>
+    <li><a href="http://myartistdna.is">MAD.is</a></li>
+    <li><a href="http://myartistdna.fm">MyAD.fm</a></li>
+       <li><a href="http://myartistdna.tv">MAD.tv</a></li>
     </ul>
     </aside>
 
     <article>
     <h5>STAY CONNECTED</h5>
     <ul>
-    <li><a href="#"><span><img src="images/facebook.gif" alt="Facebook"></span> Become a fan on Facebook</a></li>
-    <li><a href="#"><span><img src="images/twitter.gif" alt="Twitter"></span> Follow us on Twitter</a></li>
-    <li class="last"><a href="#"><span><img src="images/email.gif" alt="Email"></span> Sign up for our newsletter</a></li>
+    <li><a href="http://facebook.com/pages/MyArtistDNA/106114012796731" target="_blank"><span><img src="images/facebook.gif" alt="Facebook"></span> Become a fan on Facebook</a></li>
+    <li><a href="http://twitter.com/myartistdna" target="_blank"><span><img src="images/twitter.gif" alt="Twitter"></span> Follow us on Twitter</a></li>
+    <li><a href="#" target="_blank"><span><img src="images/email.gif" alt="Email"></span> Join Newsletter</a></li>
+
     </ul>
     </article>
 
-    <div class="logo"><a href="#"><img src="images/MYARTISTDNA_footer.gif" alt="MYARTISTDNA"></a></div>
-    <p>&copy; 2001 <a href="#">Levas Entertainment</a>, All rights reserved</p>
+    <div class="logo"><a href="http://myartistdna.is"><img src="images/MYARTISTDNA_footer.gif" alt="MYARTISTDNA"></a></div>
+    <p>&copy; 2011 <a href="http://levasent.com">Levas Entertainment</a>, All rights reserved</p>
 
 </footer>
 </section><!-- footer -->
