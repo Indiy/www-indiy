1d7b60e1e0d7b9326d986d36a926be96786d4117
Jim Lake <jim@blueskylabs.com>
2011-11-12 11:22:18 -0800

    update abbrev code

--- www/manage/setup_abbrev.php
+++ www/manage/setup_abbrev.php
@@ -10,10 +10,19 @@
     
     echo "<html><body><pre>\n";
 
+    $table = '';
+    $artist_abbrev = '';
+
     function try_abbrev($abbrev,$id)
     {
+        global $artist_abbrev;
+        global $table;
+        if( strlen( $artist_abbrev ) )
+        {
+            $abbrev = $artist_abbrev . '_' . $abbrev;
+        }
         $abbrev = strtolower($abbrev);
-        $ret = update('mydna_musicplayer','abbrev',$abbrev,'id',$id);
+        $ret = update($table,'abbrev',$abbrev,'id',$id);
         if( $ret )
             echo "Saved abbrev $abbrev for $id\n";
         return $ret;
@@ -27,9 +36,13 @@
             $abbrev = $words[0][0];
             for( $i = 1 ; $i < count($words) ; ++$i)
             {
-                $abbrev .= $words[$i][0];
-                if( try_abbrev($abbrev,$id) )
-                    return TRUE;
+                $word = $words[$i];
+                if( strlen($word) > 0 )
+                {
+                    $abbrev .= $word[0];
+                    if( try_abbrev($abbrev,$id) )
+                        return TRUE;
+                }
             }
         }
         return FALSE;
@@ -42,7 +55,7 @@
         for( $i = 1 ; $i < strlen($name) ; ++$i )
         {
             $c = $name[$i];
-            if( is_upper($c) )
+            if( ctype_upper($c) )
             {
                 $last_add = $i;
                 $abbrev .= $c;
@@ -53,17 +66,13 @@
         for( $i = $last_add + 1; $i < strlen($name) ; ++$i )
         {
             $c = $name[$i];
-            $abbrev .= $c;
-            if( try_abbrev($abbrev,$id) )
-                return TRUE;
+            if( ctype_alnum($c) )
+            {
+                $abbrev .= $c;
+                if( try_abbrev($abbrev,$id) )
+                    return TRUE;
+            }
         }
-        return FALSE;
-    }
-    
-    function is_upper($c)
-    {
-        if( $c >= 'A' && $c <= 'Z' )
-            return TRUE;
         return FALSE;
     }
     
@@ -74,24 +83,37 @@
         for( $i = 1 ; $i < strlen($name) ; ++$i )
         {
             $c = $name[$i];
-            $abbrev .= $c;
-            if( try_abbrev($abbrev,$id) )
-                return TRUE;
+            if( ctype_alnum($c) )
+            {
+                $abbrev .= $c;
+                if( try_abbrev($abbrev,$id) )
+                    return TRUE;
+            }
         }
         return FALSE;
     }
+    function cleanup_name($name)
+    {
+        for( $i = 0 ; $i < strlen($name) ; ++$i )
+        {
+            $c = $name[$i];
+            if( !ctype_alnum($c) )
+                $name[$i] = ' ';
+        }
+        return $name;
+    }
 
+    $table = 'mydna_musicplayer';
     $sql = "SELECT * FROM mydna_musicplayer WHERE abbrev IS NULL";
+    echo "sql='$sql'\n";
     $q = mq($sql);
     while( $artist = mf($q) )
     {
         $id = $artist['id'];
-        $done = FALSE;
         $name = $artist['artist'];
+        $name = cleanup_name($name);
 
         if( try_split($name,' ',$id) )
-            continue;
-        if( try_split($name,'-',$id) )
             continue;
         
         if( try_caps($name,$id) )
@@ -103,5 +125,31 @@
         echo "failed to find an abbrev for $name,$id\n";
     }
 
+
+    $table = 'mydna_musicplayer_audio';
+    $sql = "SELECT mydna_musicplayer_audio.*,mydna_musicplayer.abbrev AS artist_abbrev FROM mydna_musicplayer_audio "
+        . "JOIN mydna_musicplayer ON mydna_musicplayer_audio.artistid = mydna_musicplayer.id "
+        . "WHERE mydna_musicplayer_audio.abbrev IS NULL";
+    
+    echo "sql='$sql'\n";
+    $q = mq($sql);
+    while( $song = mf($q) )
+    {
+        $id = $song['id'];
+        $artist_abbrev = $song['artist_abbrev'];
+        $name = $song['name'];
+        $name = cleanup_name($name);
+        
+        if( try_split($name,' ',$id) )
+            continue;
+        
+        if( try_caps($name,$id) )
+            continue;
+        
+        if( try_shortened($name,$id) )
+            continue;
+        
+        echo "failed to find an song abbrev for $name,$id\n";
+    }
 
 ?>
