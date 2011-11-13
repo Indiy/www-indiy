aa1e9e5964857efc0cffb80e6449510303101515
Jim Lake <jim@blueskylabs.com>
2011-11-13 15:00:05 -0800

    cart cleanup

---
+++ www/manage/cleanup_carts.php
@@ -0,0 +1,27 @@
+<?php
+
+    require_once '../includes/config.php';
+	require_once '../includes/functions.php';	
+	if( $_SESSION['sess_userId'] == '')
+	{
+		header("Location: index.php");
+		exit();
+	}
+
+    echo "<html><body><pre>\n";
+    
+    $sql = "DELETE FROM  `mydna_musicplayer_ecommerce_cart` "
+        . "WHERE `date` < DATE_SUB( NOW() , INTERVAL 1 DAY ) ";
+        
+    $ret = mysql_query($sql);
+    if( $ret )
+    {
+        $num = mysql_affected_rows();
+        echo "Deleted $num rows.\n";
+    }
+    else
+    {
+        echo "Delete failed.\n";
+    }
+    
+?>
