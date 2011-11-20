a78954c2b46fea3ef446aeefe7de9056bc343e71
Jim Lake <jim@blueskylabs.com>
2011-11-19 16:43:24 -0800

    add copy to clipboard

--- www/manage/artist_management.php
+++ www/manage/artist_management.php
@@ -208,6 +208,30 @@ $(document).ready(setupSortableLists);
                     echo "</span>\n";
                     echo "<span class='duration'>";
                     echo "<a href='$short_link' target='_blank'>Link</a>";
+                    ?>
+<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
+width="110"
+height="14"
+id="clippy" >
+<param name="movie" value="/flash/clippy.swf"/>
+<param name="allowScriptAccess" value="always" />
+<param name="quality" value="high" />
+<param name="scale" value="noscale" />
+<param NAME="FlashVars" value="text=<?=$shortlink;?>">
+<param name="bgcolor" value="#FFFFFF">
+<embed src="/flash/clippy.swf"
+width="110"
+height="14"
+name="clippy"
+quality="high"
+allowScriptAccess="always"
+type="application/x-shockwave-flash"
+pluginspage="http://www.macromedia.com/go/getflashplayer"
+FlashVars="text=#{text}"
+bgcolor="#{bgcolor}"
+/>
+</object>
+                    <?
                     echo "</span>\n";
 					
 					if(!empty($record_artistAudio['audio']))
