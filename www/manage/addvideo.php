554600112102c530265c35c2d6bbd5851c8b6799
Jim Lake <jim@blueskylabs.com>
2011-11-20 16:53:12 -0800

    fix uploads to work on all sites

--- www/manage/addvideo.php
+++ www/manage/addvideo.php
@@ -1,15 +1,18 @@
-<?php require_once('../includes/config.php');
-	if($_SESSION['sess_userId']=="")
+<?php 
+
+    require_once '../includes/config.php';
+	require_once '../includes/functions.php';
+	if( $_SESSION['sess_userId'] == "" )
 	{
-		header("location: index.php");
+		header("Location: /index.php");
 		exit();
 	}
-	include("../includes/functions.php");
 	$database = "[p]musicplayer_video";
     $_SESSION['tabOpen']='videolist';
-	if ($_POST["WriteTags"] != "") {
-		
-		if ($_POST["id"] != "") {
+	if ($_POST["WriteTags"] != "") 
+    {
+		if ($_POST["id"] != "") 
+        {
 			$row = mf(mq("select `id`,`image`,`video` from `{$database}` where `id`='{$_POST["id"]}'"));
 			$old_logo = $row["image"];
 			$old_sound = $row["video"];
@@ -18,59 +21,74 @@
 		$video_name = my($_POST["name"]);
 		
 		// Upload Image
-		if(!empty($_FILES["logo"]["name"])){
-			if (is_uploaded_file($_FILES["logo"]["tmp_name"])) {
+		if(!empty($_FILES["logo"]["name"]))
+        {
+			if (is_uploaded_file($_FILES["logo"]["tmp_name"]))
+            {
 				$video_logo = $artistid."_".strtolower(rand(11111,99999)."_".basename(cleanup($_FILES["logo"]["name"])));
 				@move_uploaded_file($_FILES['logo']['tmp_name'], '../artists/images/' . $video_logo);
-			} else {
-				if ($old_logo != "") {
+			} 
+            else 
+            {
+				if ($old_logo != "") 
+                {
 					$video_logo = $old_logo;
 				}
 			}
-		}else{
-					$video_logo = $old_logo;
-
+		}
+        else
+        {
+            $video_logo = $old_logo;
 		}
 		
 		// Upload video
-		if(!empty($_FILES["video"]["name"])){
-			if (is_uploaded_file($_FILES["video"]["tmp_name"])) {
-				
-				$ext = explode(".",$_FILES["video"]["name"]);
+		if(!empty($_FILES["video"]["name"]))
+        {
+			if (is_uploaded_file($_FILES["video"]["tmp_name"]))
+            {
+                $tmp_file = $_FILES['video']['tmp_name'];
+				$ext = explode(".",$tmp_file);
+                $upload_ext = strtolower($ext[count($ext)-1]);
 
-				$filename = $artistid."_".strtolower(rand(11111,99999)."_video.");
-				$video_sound = $filename.$ext[count($ext)-1];
-				$video_sound_mp4 = $filename."mp4";
-				
-				@move_uploaded_file($_FILES['video']['tmp_name'], '../vid/' . $video_sound);
-				
-				$ext_name = $ext[count($ext)-1];
+				$video_sound_mp4 = $artistid . '_' . strtolower( rand(11111,99999) . '_video.mp4' );
+				$dest_file = '../vid/' . $video_sound_mp4;
 
-				if($ext_name == "mp4" || $ext_name == "MP4")
-					@system("/usr/local/bin/ffmpeg -i /home/madcom/public_html/vid/".$video_sound. " -r 20 -ar 44100 -ab 196 -b 300k -aspect 4:3  /home/madcom/public_html/vid/".$video_sound_mp4);
-				elseif($ext_name == "mov" || $ext_name == "MOV")
-					@system("/usr/local/bin/ffmpeg -i /home/madcom/public_html/vid/".$video_sound. " -ar 22050 -ab 96k -qscale 2   /home/madcom/public_html/vid/".$video_sound_mp4);
-				else
-					@system("/usr/local/bin/ffmpeg -i /home/madcom/public_html/vid/".$video_sound. " -ar 22050 -ab 96k -qscale 2   /home/madcom/public_html/vid/".$video_sound_mp4);
-
-
-
+				if( $upload_ext == "mp4" )
+                {
+					@system("/usr/local/bin/ffmpeg -i " . $tmp_file . " -r 20 -ar 44100 -ab 196 -b 300k -aspect 4:3  " . $dest_file);
+                }
+				else if( $upload_ext == "mov" )
+                {
+					@system("/usr/local/bin/ffmpeg -i " . $tmp_file . " -ar 22050 -ab 96k -qscale 2 " . $dest_file);
+				}
+                else
+                {
+					@system("/usr/local/bin/ffmpeg -i " . $tmp_file . " -ar 22050 -ab 96k -qscale 2 " . $dest_file);
+                }
 				$video_sound = $video_sound_mp4;
-			} else {
-				if ($old_sound != "") {
+			} 
+            else 
+            {
+				if ($old_sound != "") 
+                {
 					$video_sound = $old_sound;
 				}
 			}
-		}else{
-				$video_sound = $old_sound;
+		}
+        else
+        {
+            $video_sound = $old_sound;
 		}
 		
 		$tables = "artistid|name|image|video";
 		$values = "{$artistid}|{$video_name}|{$video_logo}|{$video_sound}";
 		
-		if ($_POST["id"] != "") {
+		if ($_POST["id"] != "") 
+        {
 			update($database,$tables,$values,"id",$_POST["id"]);
-		} else {
+		} 
+        else 
+        {
 			insert($database,$tables,$values);
 		}
 
@@ -89,7 +107,8 @@
 		refresh("1","?p=home");
 	}
 	
-	if ($_GET["id"] != "") {
+	if ($_GET["id"] != "") 
+    {
 		$artistid=$_REQUEST['artist_id'];
 
 		$row = mf(mq("select * from `{$database}` where `id`='{$_GET["id"]}' and `artistid`='{$artistid}'"));
@@ -99,15 +118,25 @@
 		$video_sound = $row["video"];
 
 		$head_title	=	"Edit";
-	}else{
+	}
+    else
+    {
 		$head_title	=	"Add";
 	}
 	
-	if ($video_logo != "") {
+	if ($video_logo != "")
+    {
 		$video_logo = '<img src="../artists/images/'.$video_logo.'" style="margin-top: 5px; height: 25px;" />';
 	}
 	
-	if ($video_download == "1") { $yesDownload = " checked"; } else { $noDownload = " checked"; }
+	if ($video_download == "1") 
+    { 
+        $yesDownload = " checked"; 
+    } 
+    else 
+    { 
+        $noDownload = " checked"; 
+    }
 	$video_name = stripslashes($video_name);
 ?>
 
