d89d9d20d8e71e844a71cd8960f26d8adae2aaf9
Jim Lake <jim@blueskylabs.com>
2011-11-21 21:48:45 -0800

    mp4 qtfaststart

--- www/manage/addvideo.php
+++ www/manage/addvideo.php
@@ -56,17 +56,18 @@
                 $args = "-i_qfactor 0.71 -qcomp 0.6 -qmin 10 -qmax 63 -qdiff 4 -trellis 0 -vcodec libx264 -s 640x360 -vb 300k -ab 64k -ar 44100 -threads 4";
 				if( $upload_ext == "mp4" )
                 {
-					@system("/usr/local/bin/ffmpeg -i " . $tmp_file . " " . $args . " " . $dest_file);
+					@system("/usr/local/bin/ffmpeg -i $tmp_file $args $dest_file");
                 }
 				else if( $upload_ext == "mov" )
                 {
-					@system("/usr/local/bin/ffmpeg -i " . $tmp_file . " " . $args . " " . $dest_file);
+					@system("/usr/local/bin/ffmpeg -i $tmp_file $args $dest_file");
 				}
                 else
                 {
-					@system("/usr/local/bin/ffmpeg -i " . $tmp_file . " " . $args . " " . $dest_file);
+					@system("/usr/local/bin/ffmpeg -i $tmp_file $args $dest_file");
                 }
                 @unlink($_FILES['video']['tmp_name']);
+                @system("/usr/bin/qafaststart $dest_file");
 				$video_sound = $video_sound_mp4;
 			} 
             else 
