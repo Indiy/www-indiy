a1f5abe23e0cfec402ff4802a48e8ce05ca6d88a
Jim Lake <jim@blueskylabs.com>
2011-12-18 18:38:08 -0800

    socialize admin pages

--- www/manage/socialize.php
+++ www/manage/socialize.php
@@ -20,7 +20,7 @@
 
 <div id="popup">
     <div class="addcontent">
-        <h2 class="title"  id="demonstrations">Add Artist</h2>
+        <h2 class="title"  id="demonstrations">Socialize</h2>
         <form id="none"  onsubmit='return false;'>
             <div id="form_field">
             <div class="clear"></div>
@@ -31,8 +31,9 @@
             <p>Warning - You can not undo once you publish</p>
             <div class="clear"></div>
 
-            <input type="radio" name="fb_or_tw" value="facebook" class="radio" /> Facebook
+            Select a platform:
             <input type="radio" name="fb_or_tw" value="twitter" class="radio" /> Twitter
+            <input type="radio" name="fb_or_tw" value="facebook" class="radio" /> Facebook
             <div class="clear"></div>
             
             <button id='socialize_publish' class="submit" onclick='onSocializePublish();'>Publish</button>
