cf02f6c90668ae6cd38a524d21d518518cc66e86
Jim Lake <jim@blueskylabs.com>
2011-12-19 14:32:04 -0800

    temp code

--- www/Login_Twitbook/config/fbconfig.php
+++ www/Login_Twitbook/config/fbconfig.php
@@ -1,6 +1,13 @@
 <?php
-define('APP_ID', '180862031990017');
-define('APP_SECRET', 'e8dc6fa4058b881fc2c4ee9ceead9f0e');
+
+// MyArtistDNA code
+//define('APP_ID', '180862031990017');
+//define('APP_SECRET', 'e8dc6fa4058b881fc2c4ee9ceead9f0e');
+
+// Temp Test Code
+define('APP_ID', '256680924395123');
+define('APP_SECRET', 'aeed84d28520b93930a3f83ccee6dd1f');
+
 
 //define('APP_ID', '200922729964116');
 //define('APP_SECRET', '5d2ba4c0eac91f6c1c17f57095ca8a3d');
