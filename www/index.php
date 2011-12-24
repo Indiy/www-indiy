1526b4c40308cb10aee3c9ebe3be59e5136aa65f
Jim Lake <jim@blueskylabs.com>
2011-12-24 11:36:53 -0800

    fix landing on madd3v

--- www/index.php
+++ www/index.php
@@ -7,59 +7,51 @@
         die();
     }
 
-	$loadUsername = explode(".", $_SERVER["HTTP_HOST"]);
-	if ($loadUsername[0] == "www" || $loadUsername[0] == "myartistdna") {
+	$host_parts = explode(".", $_SERVER["HTTP_HOST"]);
+	if( array_len($host_parts) < 3 || $host_parts[0] == "www" ) 
+    {
 		$artist_url = $_GET["url"];
-	} else {
-		$artist_url = $loadUsername[0];
+	} 
+    else 
+    {
+		$artist_url = $host_parts[0];
 	}
 
-	if (is_file("install.php")) {
-	
-		include('includes/config.php');
-		include('includes/functions.php');
-		include("install.php");
-		unlink("install.php");
-		refresh("2","");
-		echo "<p>Installing...</p>";
-	
-	} else {
+    if ($_GET["p"] != "") 
+    {
+        include('includes/config.php');
+        include('includes/functions.php');
+    
+        // call the function to include the "frame.tpl" file as a string
+        $string = get_include_contents('includes/frame.php');
+        // break apart the "frame.tpl" to pull the header and footer out
+        $displayframe = explode("{inject}", $string);
+        // header variable
+        $header = $displayframe[0];
+        // footer variable
+        $footer = $displayframe[1];		
 
-		if ($_GET["p"] != "") {
-
-			include('includes/config.php');
-			include('includes/functions.php');
-		
-			// call the function to include the "frame.tpl" file as a string
-			$string = get_include_contents('includes/frame.php');
-			// break apart the "frame.tpl" to pull the header and footer out
-			$displayframe = explode("{inject}", $string);
-			// header variable
-			$header = $displayframe[0];
-			// footer variable
-			$footer = $displayframe[1];		
-
-			//// LOAD FRAME /////////////////////////////////////////////////////////
-			if ($_GET["p"] == "index") {
-				include("pages/index.php");
-			} else {
-				echo $header; // Header
-				include('pages/'.$_GET["p"].'.php'); // Body
-				echo $footer; // Footer
-			}
-			
-		} else if ($_GET["url"] != "" || $artist_url != "") {
-			
-			include('includes/config.php');
-			include('includes/functions.php');
-			include('jplayer/index.php');
-		
-		} else {
-
-			include('includes/config.php');
-			include('includes/functions.php');		
-			//include('home.php');
-            include 'landing.html';
-		}
-	}
+        //// LOAD FRAME /////////////////////////////////////////////////////////
+        if ($_GET["p"] == "index") {
+            include("pages/index.php");
+        } else {
+            echo $header; // Header
+            include('pages/'.$_GET["p"].'.php'); // Body
+            echo $footer; // Footer
+        }
+        
+    } 
+    else if( $_GET["url"] != "" || $artist_url != "" )
+    {
+        include('includes/config.php');
+        include('includes/functions.php');
+        include('jplayer/index.php');
+    }
+    else
+    {
+        include('includes/config.php');
+        include('includes/functions.php');		
+        //include('home.php');
+        include 'landing.html';
+    }
 ?>
