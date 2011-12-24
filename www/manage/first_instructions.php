9acf78c2355bc2f3b756a6a66b6cf7fc7d404e06
Jim Lake <jim@blueskylabs.com>
2011-12-23 23:25:46 -0800

    first instructions

--- www/manage/first_instructions.php
+++ www/manage/first_instructions.php
@@ -1,19 +1,43 @@
 <div id="popup">
     <div class='top_bar'>
-    <h2>Welcome to MyArtistDNA</h2>
+    <h2>Welcome</h2>
     <button onclick='$.facebox.close();'>CLOSE</button>
     </div>
 
     <div class='top_blue_bar'></div>
     <div class='top_sep'></div>
-    <form id="none"  onsubmit='return false;'>
-        <div class='input_container'>
-            <div class='left_label'>Artist</div>
-            <input id='artist' type="text" name="artist" value="" class='right_text' />
+
+    <div class='line_title'>Let's start designing!</div>
+    <div class='number_container'>
+        <div class='left_number'>1</div>
+        <div class='right_title_detail'>
+            <div class='right_title'>Make a name for youself</div>
+            <div class='right_detail'>
+            Click on Store > Details to edit your title and description
+            </div>
         </div>
-    </form>
+    </div>
+    <div class='number_container'>
+        <div class='left_number'>2</div>
+        <div class='right_title_detail'>
+            <div class='right_title'>Tell your story</div>
+            <div class='right_detail'>
+            Click on Pages > Add to create an About page
+            </div>
+        </div>
+    </div>
+    <div class='number_container'>
+        <div class='left_number'>3</div>
+        <div class='right_title_detail'>
+            <div class='right_title'>Brand your store</div>
+            <div class='right_detail'>
+            Click on Design to change your layout, color scheme, background, and more.
+            </div>
+        </div>
+    </div>
     
-    <div id='status'></div>
+    <div class='need_help'>Need more help?</div>
+    
     <div class='bottom_sep'></div>
     <div class='bottom_blue_bar'></div>
 </div>
