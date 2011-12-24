3de384c96af44222a4d920f9a76b33d008acff00
Jim Lake <jim@blueskylabs.com>
2011-12-23 21:38:24 -0800

    admin tooltips

--- www/manage/js/manage.js
+++ www/manage/js/manage.js
@@ -453,7 +453,7 @@ function hoverInQuestion(event)
 
     var new_offset = $(event.target).offset();
     new_offset.left -= $('#question_tooltip').width()/2 - 20;
-    new_offset.top -= $('#question_tooltip').height() + 5;
+    new_offset.top -= $('#question_tooltip').height() + 15;
     $('#question_tooltip').offset(new_offset);
     
 }
