1a90723c9a80641b924cf94533b1c184f3ea6cb4
Alphonso Morris <amorris@levasent.com>
2011-12-28 15:03:42 -0500

    Artists.php update

--- www/artists.php
+++ www/artists.php
@@ -27,40 +27,40 @@ include "header.php";
 	
     <div id="artistshome">
     	<ul id="list" class="image-grid">
-        <li data-id="id-1" class="rock"><a href="#"><img src="images/artists_01.jpg" alt="">
+        <li data-id="id-1" ><a href="#"><img src="images/artists_01.jpg" alt="">
         <span>Black Violin</span></a></li>
         
-        <li data-id="id-2" class="pop"><a href="#"><img src="images/artists_02.jpg" alt="">
+        <li data-id="id-2" ><a href="#"><img src="images/coolMent.jpg" alt="">
         <span>Cool Mentality</span></a></li>
         
-        <li data-id="id-3" class="hiphop"><a href="#"><img src="images/artists_03.jpg" alt="">
+        <li data-id="id-3" ><a href="#"><img src="images/wondaland.jpg" alt="">
         <span>Woodlands Arts Society</span></a></li>
         
-        <li data-id="id-4" class="dance"><a href="#"><img src="images/artists_04.jpg" alt="">
+        <li data-id="id-4" ><a href="#"><img src="images/artists_04.jpg" alt="">
         <span>Kithe Brewster</span></a></li>
         
-        <li data-id="id-5" class="country"><a href="#"><img src="images/artists_05.jpg" alt="">
+        <li data-id="id-5" ><a href="#"><img src="images/bc.jpg" alt="">
         <span>Babylon Cartel</span></a></li>
         
-        <li data-id="id-6" class="pop"><a href="#"><img src="images/artists_06.jpg" alt="">
+        <li data-id="id-6" ><a href="#"><img src="images/artists_03.jpg" alt="">
         <span>Hireosonic</span></a></li>
 		
-		<li data-id="id-7" class="pop"><a href="http://navegante.myartistdna.com"><img src="images/nvg.jpg" alt="">
+		<li data-id="id-7" ><a href="http://navegante.myartistdna.com"><img src="images/nvg.jpg" alt="">
         <span>Navegante</span></a></li>
         
-        <li data-id="id-8" class="pop"><a href="http://youngchris.myartistdna.com"><img src="images/youngc.jpg" alt="">
+        <li data-id="id-8" ><a href="http://youngchris.myartistdna.com"><img src="images/youngc.jpg" alt="">
         <span>Young Chris</span></a></li>
         
-        <li data-id="id-9" class="pop"><a href="http://marcecko.myartistdna.com"><img src="images/marc.jpg" alt="">
+        <li data-id="id-9" ><a href="http://marcecko.myartistdna.com"><img src="images/marc.jpg" alt="">
         <span>Marc Ecko</span></a></li>
         
-        <li data-id="id-10" class="pop"><a href="#"><img src="images/blakroc.jpg" alt="">
+        <li data-id="id-10" ><a href="#"><img src="images/blakroc.jpg" alt="">
         <span>Blakroc</span></a></li>
         
-        <li data-id="id-11" class="pop"><a href="#"><img src="images/mmjb.jpg" alt="">
+        <li data-id="id-11" ><a href="#"><img src="images/mmjb.jpg" alt="">
         <span>Money Making Jam Boys</span></a></li>
         
-  		<li data-id="id-12" class="pop"><a href="#"><img src="images/jen.jpg" alt="">
+  		<li data-id="id-12" ><a href="#"><img src="images/jen.jpg" alt="">
         <span>Jen</span></a></li>
         
         </ul>
