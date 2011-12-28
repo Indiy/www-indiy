0b14a961d1031dad9293de3b44ddcc4bd97863de
Alphonso Morris <amorris@levasent.com>
2011-12-28 13:40:09 -0500

    Homepage Edit

--- www/home.php
+++ www/home.php
@@ -10,18 +10,7 @@
 		<div class='header_2'><a href="http://navegante.myartistdna.com/"><img alt="Navegante" height="257" width="942" src="images/header_images/02.jpg" /></a></div>
 		<div class='header_3'><a href="http://ladygaga.myartistdna.com/"><img alt="Lady Gaga" height="257" width="942" src="images/header_images/03.jpg" /></a></div>
 	</div>
-		
-    <div class="features">
-    <!--<ul id="accordion-slider">
-
-    <li><a href="http://navegante.myartistdna.fm" target="_blank"><img src="images/myartistdna_01.jpg" alt=""></a></li>
-
-    <li><a href="http://hierosonic.myartistdna.fm" target="_blank"><img src="images/myartistdna_12.jpg" alt=""></a></li>
-
-    <li><a href="http://youngchris.myartistdna.fm" target="_blank"><img src="images/myartistdna_09.jpg" alt=""></a></li>    
-    </ul>-->
-    </div><!-- features -->
-    
+		    
     
     <div class="signup">
     <h3>AN INDEPENDENT BRANDING NETWORK</h3>
@@ -70,23 +59,23 @@
         <h2>Artists who use MY<span>ARTIST</span>DNA</h2>
     
         <ul>
-        <li><a href="http://navegante.myartistdna.com"><img src="images/artists_01.jpg" alt="">
+        <li><a href="http://navegante.myartistdna.com"><img src="images/nvg.jpg" alt="">
         <span>Navegante</span></a></li>
         
-        <li><a href="http://youngchris.myartistdna.com"><img src="images/artists_02.jpg" alt="">
+        <li><a href="http://youngchris.myartistdna.com"><img src="images/youngc.jpg" alt="">
         <span>Young Chris</span></a></li>
         
-        <li><a href="http://hierosonic.myartistdna.com"><img src="images/artists_03.jpg" alt="">
-        <span>Hierosonic</span></a></li>
+        <li><a href="http://marcecko.myartistdna.com"><img src="images/marc.jpg" alt="">
+        <span>Marc Ecko</span></a></li>
         
         <li><a href="#"><img src="images/artists_05.jpg" alt="">
-        <span>Vanessa Cantave</span></a></li>
+        <span>Blakroc</span></a></li>
         
-        <li><a href="#"><img src="images/artists_06.jpg" alt="">
-        <span>N.J. State of Mind</span></a></li>
+        <li><a href="#"><img src="images/mmjb.jpg" alt="">
+        <span>Money Making Jam Boys</span></a></li>
         
-  		 <li><a href="http://kithebrewster.myartistdna.com"><img src="images/artists_04.jpg" alt="">
-        <span>Kithe Brewster</span></a></li>
+  		 <li><a href="#"><img src="images/jen.jpg" alt="">
+        <span>Jen</span></a></li>
         
         </ul>
         
