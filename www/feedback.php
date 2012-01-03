0e7265bc3cf17fc7718e42e924bb59e606c18e79
Alphonso Morris <amorris@levasent.com>
2012-01-03 08:48:39 -0500

    Copy edit

---
+++ www/feedback.php
@@ -0,0 +1,135 @@
+<?php
+include "header.php";
+?>
+
+<section id="wrapper">
+<section id="content">
+
+    <div id="faq">
+         <h2><a href="#">FAQ</a></h2>
+         <div class="content_inner">
+         <div class="questions"> 
+         <h3>Our most frequently asked questions</h3>
+         <ul>
+
+         <li><span class="number">1</span><span class="details"><a href="#">What is MyArtistDNA?</a></span></li>
+         <li><span class="number">2</span><span class="details"><a href="#">How much does MyArtistDNA cost?</a></span></li>
+         <li><span class="number">3</span><span class="details"><a href="#">I already has a site, how can I integrate it with MyArtistDNA?</a></span></li>
+         <li><span class="number">4</span><span class="details"><a href="#">What's the maximum about of content an user can upload?</a></span></li>
+         <li><span class="number">5</span><span class="details"><a href="#">What type and size of files can I upload?</a></span></li>
+         <li><span class="number">6</span><span class="details"><a href="#">Can fans download music?</a></span></li>
+         <li><span class="number">7</span><span class="details"><a href="#">Can I collect fans emails?</a></span></li>
+         <li><span class="number">8</span><span class="details"><a href="#">Who's using MyArtistDNA?</a></span></li>
+         <li><span class="number">9</span><span class="details"><a href="#">Can I point my own domain to MyArtistDNA site?</a></span></li>
+         <li><span class="number">10</span><span class="details"><a href="#">Can I sell anything?</a></span></li>
+		 <li><span class="number">11</span><span class="details"><a href="#">What size images do you suggest to upload?</a></span></li>
+         <li><span class="number">12</span><span class="details"><a href="#">Can I add info about my band?</a></span></li>
+         <li><span class="number">13</span><span class="details"><a href="#">Can I add my social media?</a></span></li>
+		 </ul>
+		</div>
+        
+        <div class="checkout">
+        <h5>Can't find the answer <br /> you’re looking for?</h5>
+        <p>Visit our Help page to contact our support team directly. <a href="tour.php">GO</a></p>
+        </div>
+        </div>
+        
+        <div class="subcontent">
+        <div class="setlist">
+        <article>
+		<span class="details"><p>What is MyArtistDNA?<br /></span>
+        A home for artists' brands and discovery destination for fan's. MyArtistDNA is a movement that puts the control of the artists back into artist to inspire to be independent. We provide the outlet for Artists' brands to build the way they intended. We are your partner in this journey. Be heard. Be seen. Be independent. </p>
+        <p><a href="#faq">Back to top</a></p>
+        </article>
+        
+        <article>
+        <p>How much does MyArtistDNA cost?<br />
+        Nothing if you choose us as your partner. We call this the OPT IN option. By opting in we will guide you down this success path and equip you with the tools to succeed. You can OPT OUT. OPT OUT is an option that you pay a monthly fee to use the software and gives you the software with limited features to deploy. </p>
+        <p><a href="#faq">Back to top</a></p>
+        </article>
+        
+        <article>
+        <p>I already have a site, how can I integrate it with MyArtistDNA?<br />
+        We encourage you use MyArtistDNA as your home of your brand. Upload all your content. Use other outlets as your marketing tools. Your account can be, replace or accompany your personal website.</p>
+        <p><a href="#faq">Back to top</a></p>
+        </article>
+        
+        <article>
+        <p>What's the maximum amount of content an user can upload?<br />
+       </p>
+        <p><a href="#faq">Back to top</a></p>
+        </article>
+        
+         <article>
+        <p>What type and size of files can I upload?<br />
+        </p>
+        <p><a href="#faq">Back to top</a></p>
+        </article>
+        
+         <article >
+        <p>Can fans download music?<br />
+       YESS!!!</p>
+        <p><a href="#faq">Back to top</a></p>
+        </article>
+        
+         <article >
+        <p>Can I collect fans emails?<br />
+        YESS!!!</p>
+        <p><a href="#faq">Back to top</a></p>
+        </article>
+        
+         <article >
+        <p>Who's using MyArtistDNA?<br />
+        Artists and fans. The Artists is our customer. Today we have focused on music and fashion. </p>
+        <p><a href="#faq">Back to top</a></p>
+        </article>
+        
+         <article >
+        <p>Can I point my own domain to MyArtistDNA site? <br />
+        Yes! Or your account can be your website.</p>
+        <p><a href="#faq">Back to top</a></p>
+        </article>
+        
+         <article >
+        <p>Can I sell anything?<br />
+        YES!!! Each account comes with their own store.
+</p>
+        <p><a href="#faq">Back to top</a></p>
+        </article>
+        
+         <article >
+        <p>What size images do you suggest to upload?<br />
+      </p>
+        <p><a href="#faq">Back to top</a></p>
+        </article>
+        
+         <article >
+        <p>Can I add info about my band?<br />
+        YES!!! Each account has your own tabs you can make and also we are super believers in METADATA! The more detailed info you can give us the better for YOU!!! You must tag and fingerprint all your work properly so people can find you in unlimited ways!</p>
+        <p><a href="#faq">Back to top</a></p>
+        </article>
+
+  <article >
+        <p>Can I add my social media?<br />
+       YES!!! We have social media integration.</p>
+        <p><a href="#faq">Back to top</a></p>
+        </article>
+        
+         <article class="last">
+        <p>Is there anyway I can track my stats?<br />
+       YES!!! We have detailed analytics for your account that will tell you a lot of information. See what's popular and control the distribution and order of content based off feedback. 
+</p>
+        <p><a href="#faq">Back to top</a></p>
+        </article>
+
+        </div>
+        </div>
+    </div><!-- faq -->
+	
+
+</section><!-- content -->
+</section><!-- wrapper -->
+
+<?php
+include "footer.php";
+?>
