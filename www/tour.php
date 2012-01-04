1dbe6f027d2d25503bacb7167df76567b856b787
Alphonso Morris <amorris@levasent.com>
2012-01-04 13:27:16 -0500

    Video Update

--- www/tour.php
+++ www/tour.php
@@ -12,8 +12,7 @@ include ('header.php');
 		<div class="videos">
 		<!-- first try HTML5 playback: if serving as XML, expand `controls` to `controls="controls"` and autoplay likewise -->
 <!-- warning: playback does not work on iOS3 if you include the poster attribute! fixed in iOS4.0 -->
-<video controls>
-	<!-- MP4 must be first for iPad! -->
+<video width="638" height="358" controls>	<!-- MP4 must be first for iPad! -->
    <source src="_Master.mp4" type="video/mp4" />
    <source src="_Master.webm" type="video/webm" />
    <!-- <source src="http://www.myartistdna.com/mad.ogv" type="video/ogg" /> -->
