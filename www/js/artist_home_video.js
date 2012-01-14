1a94028eec157a26b3b665caf1cee5a571a7514d
Jim Lake <jim@blueskylabs.com>
2012-01-13 23:21:53 -0800

    new video player

--- www/js/artist_home_video.js
+++ www/js/artist_home_video.js
@@ -15,11 +15,12 @@ function showVideo(n)
 
 
     var video_file = video.video_file;
+    var poster = video.image_file;
 
     var html = '';
     
-    html += '<div class="video-js-box vim-css">';
-    html += '<video id="mad_video_1" class="video-js" controls="controls" preload="auto" poster="/images/mad_poster.png">';
+    html += '<div class="video-js-box vim-css" style="width:100%; height: 100%;">';
+    html += '<video id="mad_video_1" class="video-js" width="100%" height="100%" controls="controls" preload="auto" poster="' + poster + '">';
     html += '<source src="' + video_file + '" type="video/mp4" />';
     html += '<object id="flash_fallback_1" class="vjs-flash-fallback" width="640" height="264" type="application/x-shockwave-flash" data="http://releases.flowplayer.org/swf/flowplayer-3.2.1.swf">';
     
