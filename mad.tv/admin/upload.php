de179b1795442e2c1944228604b6b6257ba9cfd5
Jim Lake <jim@blueskylabs.com>
2012-01-25 17:00:45 -0800

    mad.tv

---
+++ mad.tv/admin/upload.php
@@ -0,0 +1,21 @@
+<?
+
+?>
+
+<html>
+<body>
+<h1>Fancy Upload interface</h1>
+
+<form method='post'>
+    Name: <input name='name'><br/>
+    Artist: <input name='artist'><br/>
+    Logo: <input type='file' name='logo'><br/>
+    Poster: <input type='file' name='poster'><br/>
+    Video File: <input type='file' name='video_file'><br/>
+    <br/>
+    <input type='submit' value='Submit'><br/>
+</form>
+
+</body>
+</html>
+
