472e8d5367ff814dbee9b1a0a3d4d3a727f7c42c
Jim Lake <jim@blueskylabs.com>
2012-02-24 16:28:13 -0800

    content edit raw html

--- www/manage/addcontent.php
+++ www/manage/addcontent.php
@@ -158,9 +158,72 @@ function onReady()
                                            handleSubmit: false,
                                            toolbar: toolbar_config
                                            });
+    
+    g_editor.on('toolbarLoaded',onToolbarLoaded,g_editor,true);
     g_editor.render();
+    
     setupQuestionTolltips();
 }
+
+function onToolbarLoaded()
+{
+    var codeConfig = {
+        type: 'push', 
+        label: 'Edit HTML Code', 
+        value: 'editcode'
+    };
+    this.toolbar.addButtonToGroup(codeConfig, 'insertitem');
+    this.toolbar.on('editcodeClick', function() 
+                    {
+                        var ta = this.get('element'),
+                        iframe = this.get('iframe').get('element');
+                        
+                        if (state == 'on') {
+                            state = 'off';
+                            this.toolbar.set('disabled', false);
+                            this.setEditorHTML(ta.value);
+                            if (!this.browser.ie) {
+                                this._setDesignMode('on');
+                            }
+                        
+                            Dom.removeClass(iframe, 'editor-hidden');
+                            Dom.addClass(ta, 'editor-hidden');
+                            this.show();
+                            this._focusWindow();
+                        } else {
+                            state = 'on';
+                            this.cleanHTML();
+                            Dom.addClass(iframe, 'editor-hidden');
+                            Dom.removeClass(ta, 'editor-hidden');
+                            this.toolbar.set('disabled', true);
+                            this.toolbar.getButtonByValue('editcode').set('disabled', false);
+                            this.toolbar.selectButton('editcode');
+                            this.dompath.innerHTML = 'Editing HTML Code';
+                            this.hide();
+                        }
+                    return false;
+                    }, this, true);
+    
+    this.on('cleanHTML', function(ev) 
+            {
+                this.get('element').value = ev.html;
+            }, this, true);
+    
+    this.on('afterRender', function() 
+            {
+                var wrapper = this.get('editor_wrapper');
+                wrapper.appendChild(this.get('element'));
+                this.setStyle('width', '100%');
+                this.setStyle('height', '100%');
+                this.setStyle('visibility', '');
+                this.setStyle('top', '');
+                this.setStyle('left', '');
+                this.setStyle('position', '');
+                
+                this.addClass('editor-hidden');
+            }, this, true);
+}
+
 
 $(document).ready(onReady);
 
