11d3623a97f5afaace4824936e934956b2949a8e
Jim Lake <jim@blueskylabs.com>
2012-03-11 22:43:35 -0700

    update mad.fm styles

--- mad.fm/data/stream_info.php
+++ mad.fm/data/stream_info.php
@@ -5,6 +5,7 @@ $FILE = "/tmp/mad_fm.json";
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
+header("Access-Control-Allow-Origin: *");
 
 $json = file_get_contents($FILE);
 print $json;
