aba152825241d62bb9d2aa70e0b0004f647cd874
Jim Lake <jim@blueskylabs.com>
2012-03-24 14:26:10 -0700

    mad.fm genre updates

--- mad.fm/js/splash.js
+++ mad.fm/js/splash.js
@@ -4,8 +4,52 @@ function onSplashReady()
 {
     $('#play_container').mouseover(mouseoverPlaylist);
     $('#play_container').mouseout(mouseoutPlaylist);
+    
+    if( typeof(g_genreList) == "undefined" )
+        loadGenreList();
+    else
+        renderGenreList();
 }
 $(document).ready(onSplashReady);
+
+function loadGenreList()
+{
+    jQuery.ajax(
+    {
+        type: 'GET',
+        url: "http://www.myartistdna.fm/data/stream_info.php",
+        dataType: 'json',
+        success: function(data) 
+        {
+            g_genreList = data.genre_list;
+            renderGenreList();
+        },
+        error: function()
+        {
+        }
+    });
+}
+
+function renderGenreList()
+{
+    $('#genre_container').empty();
+    var cls = 'top';
+    for( var i = 0 ; i < g_genreList.length ; ++i )
+    {
+        var g = g_genreList[i];
+        var html = "<a href='player.html?genre=" + g + "'>";
+        html += "<div class='item " + cls + "'>";
+        html += "<div class='label'>I WANT " + g + "</div>";
+        html += "<div class='icon'></div>";
+        html += "</div>";
+        html += "</a>";
+        if( cls == 'top' )
+            cls = 'second';
+        else
+            cls = 'reg';
+        $('#genre_container').append(html);
+    }
+}
 
 var g_playlistTimer = false;
 var g_playlistOpen = false;
--- mad.tv/data/stream_info.php
+++ mad.tv/data/stream_info.php
@@ -1,7 +1,5 @@
 <?php
 
-    $FILE = 
-
     header("Content-Type: application/json");
     header("Cache-Control: no-cache");
     header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
