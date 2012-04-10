2ad28bee21464b8079430953c5847a5e8733e482
Alphonso Morris <amorris@levasent.com>
2012-04-10 18:37:38 -0500

    CSS edit

--- www/pricing.php
+++ www/pricing.php
@@ -55,7 +55,7 @@ include "header.php";
 		</aside>
 		
 		<aside class="platinum"> 
-			<h2>MYARTISTDNA.IS</h2> 
+			<h2><span class="yr">MYARTISTDNA.IS</span></h2> 
 			<div class="content">
 			<p>Mauris ut arcu et turpis aliquet vulputate a ut libero. Integer sit amet nulla enim, non luctus sem. Sed sed dui lacus.</p> 
 			<p><span>Aspire to be Independent.</span></p>
