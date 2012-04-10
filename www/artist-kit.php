d8eabf9657ef89546e9b64568b954fe6542df8e1
Alphonso Morris <amorris@levasent.com>
2012-04-10 14:11:07 -0500

    New Page

---
+++ www/artist-kit.php
@@ -0,0 +1,82 @@
+<?php
+include "header.php";
+?>
+
+<section id="wrapper">
+<section id="content">
+	
+	
+	
+	<div id="artists">
+	<div class="heading">
+    <h2>MUSIC</h2>
+    
+    <!-- <div class="sort">
+        <a class="sub" href="#">Hip Hop</a>
+        </div> dropdown -->
+    </div>
+	
+    <div class="features">
+   
+    <img src="images/musicBanner.jpg" alt="">
+  
+    </div><!-- features -->
+    
+    <div class="signup">
+    <h3>Experience MyArtistDNA</h3>
+    <div class="button"><a href="http://navegante.myartistdna.com">EXPLORE NOW</a></div>
+    </div><!-- signup -->
+
+	
+    <div id="artistshome">
+	<h2>ARTISTS USING MY<span>ARTIST</span>DNA</h2>
+    	<ul>
+         <li><img src="images/bv.jpg" alt="">
+        <span>Black Violin</span></li>
+        
+        <li><img src="images/coolMent.jpg" alt="">
+        <span>Cool Mentality</span></li>
+        
+        <li><img src="images/wondaland.jpg" alt="">
+        <span>Wondaland Arts Society</span></li>
+        
+        <li><img src="images/artists_04.jpg" alt="">
+        <span>Kithe Brewster</span></li>
+        
+        <li><img src="images/bc.jpg" alt="">
+        <span>Babylon Cartel</span></li>
+        
+        <li><img src="images/artists_03.jpg" alt="">
+        <span>Hierosonic</span></li>
+		
+		<li><img src="images/nvg.jpg" alt="">
+        <span>Navegante</span></li>
+        
+        <li><img src="images/youngc.jpg" alt="">
+        <span>Young Chris</span></li>
+        
+        <li><img src="images/marc.jpg" alt="">
+        <span>Marc Ecko</span></li>
+        
+        <li><img src="images/blakroc.jpg" alt="">
+        <span>Blakroc</span></li>
+        
+        <li><img src="images/mmjb.jpg" alt="">
+        <span>Money Making Jam Boys</span></li>
+        
+  		        
+        </ul>
+	</div>	
+    </div><!-- artists -->
+    
+    <div class="signup">
+    <h3>Be heard, be seen, be independent.</h3>
+    <div class="button"><a href="#" onclick="showSignup();">SIGN UP NOW</a></div>
+    </div><!-- signup -->
+
+</section><!-- content -->
+</section><!-- wrapper -->
+
+<?php
+include "footer.php";
+?>
