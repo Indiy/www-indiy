3cf864c73b38050f46a8f595929f2fc4ebaee3d2
Alphonso Morris <amorris@levasent.com>
2012-04-10 20:29:27 -0500

    logo edit

--- www/artist-kit.php
+++ www/artist-kit.php
@@ -28,22 +28,22 @@ include "header.php";
 
 	
     <div id="artistshome">
-	<h2><img src="images/madcom-logo.jpg" alt="">&nbsp;&nbsp;&nbsp;STEP 1 Publish your content: MY<span>ARTIST</span>DNA.com</h2>
+	<h2><img src="images/madcom-logo.jpg" alt="">&nbsp;&nbsp;&nbsp;STEP 1: Publish your content</h2>
     	<ul>
         <span>This is the on boarding publishing tool that allows you to create a presence for your audio, video or image art in seconds</span></li>
         </ul>
 	<br/><br/>
-	<h2><a href="http://myartistdna.is" target="_blank"><img src="images/madis-logo.jpg" alt=""></a>&nbsp;&nbsp;&nbsp;STEP 2 Brand your content: <a href="http://myartistdna.is" target="_blank">MY<span>ARTIST</span>DNA.is</a></h2>
+	<h2><a href="http://myartistdna.is" target="_blank"><img src="images/madis-logo.jpg" alt=""></a>&nbsp;&nbsp;&nbsp;STEP 2: Brand your content</a></h2>
     	<ul>
         <span>Your effort plus our experience allows you to maximize your brand awareness. This is the branding agency that helps users brand their art</span></li>
         </ul>
 	<br/><br/>
-	<h2><a href="http://myartistdna.fm" target="_blank"><img src="images/madfm-logo.jpg" alt=""></a>&nbsp;&nbsp;&nbsp;STEP 3 Be Heard: <a href="http://myartistdna.fm" target="_blank">MY<span>ARTIST</span>DNA.fm</a></h2>
+	<h2><a href="http://myartistdna.fm" target="_blank"><img src="images/madfm-logo.jpg" alt=""></a>&nbsp;&nbsp;&nbsp;STEP 3: Be Heard</h2>
     	<ul>
         <span>Set up a MyArtistDNA.com account, promote your account and gain enough "Loves" from your fans to vote your audio content onto MyArtistDNA.fm. This is an audio streaming network that brings the best audio content from myartistdna.com to create limitless audio stream channels for fans to listen.<br/><br/></span></li>
         </ul>
 	<br/><br/><br/>
-	<h2><a href="http://myartistdna.tv" target="_blank"><img src="images/madtv-logo.jpg" alt=""></a>&nbsp;&nbsp;&nbsp;STEP 4 Be Seen: <a href="http://myartistdna.tv" target="_blank">MY<span>ARTIST</span>DNA.tv</a></h2>
+	<h2><a href="http://myartistdna.tv" target="_blank"><img src="images/madtv-logo.jpg" alt=""></a>&nbsp;&nbsp;&nbsp;STEP 4 Be Seen</h2>
     	<ul>
         <span>Set up a MyArtistDNA.com account, promote your account and gain enough "Loves" from your fans to vote your video content onto MyArtistDNA.tv. This is a video streaming network that brings the best video content from myartistdna.com to create limitless video stream channels for fans to watch.</span></li>
         </ul>
