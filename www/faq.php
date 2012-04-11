1e264f9c143b77b48a3604f6b2b2ced10aebc4d5
Alphonso Morris <amorris@levasent.com>
2012-04-11 13:21:56 -0500

    FAQ clean up

--- www/faq.php
+++ www/faq.php
@@ -5,127 +5,146 @@ include "header.php";
 <section id="wrapper">
 <section id="content">
 
-    <div id="faq">
-         <h2><a href="#">FAQ</a></h2>
-         <div class="content_inner">
-         <div class="questions"> 
-         <h3>Our most frequently asked questions</h3>
-         <ul>
+<div id="faq">
+<h2><a href="#">FAQ</a></h2>
+<div class="content_inner">
+<div class="questions"> 
+<h3>Our most frequently asked questions</h3>
+<ul>
 
-         <li><span class="number">1</span><span class="details"><a href="#anchor_what">What is MyArtistDNA?</a></span></li>
-         <li><span class="number">2</span><span class="details"><a href="#anchor_how_much">How much does MyArtistDNA cost?</a></span></li>
-         <li><span class="number">3</span><span class="details"><a href="#anchor_already_have_site">I already has a site, how can I integrate it with MyArtistDNA?</a></span></li>
-         <li><span class="number">4</span><span class="details"><a href="#anchor_max">What's the maximum about of content an user can upload?</a></span></li>
-         <li><span class="number">5</span><span class="details"><a href="#anchor_type">What type and size of files can I upload?</a></span></li>
-         <li><span class="number">6</span><span class="details"><a href="#anchor_download">Can fans download music?</a></span></li>
-         <li><span class="number">7</span><span class="details"><a href="#anchor_fan_email">Can I collect fans emails?</a></span></li>
-         <li><span class="number">8</span><span class="details"><a href="#anchor_who">Who's using MyArtistDNA?</a></span></li>
-         <li><span class="number">9</span><span class="details"><a href="#anchor_domain">Can I point my own domain to MyArtistDNA site?</a></span></li>
-         <li><span class="number">10</span><span class="details"><a href="#anchor_sell">Can I sell anything?</a></span></li>
-		 <li><span class="number">11</span><span class="details"><a href="#anchor_size">What size images do you suggest to upload?</a></span></li>
-         <li><span class="number">12</span><span class="details"><a href="#anchor_band_info">Can I add info about my band?</a></span></li>
-         <li><span class="number">13</span><span class="details"><a href="#anchor_social">Can I add my social media?</a></span></li>
-		 </ul>
-		</div>
-        
-        <div class="checkout">
-        <h5>Can't find the answer <br /> you’re looking for?</h5>
-        <p>Visit our Help page to contact our support team directly. <a href="tour.php">GO</a></p>
-        </div>
-        </div>
-        
-        <div class="subcontent">
-        <div class="setlist">
-        <article id='anchor_what'>
-		<span class="details"><p>What is MyArtistDNA?<br /></span>
-        A home for artists' brands and discovery destination for fan's. MyArtistDNA is a movement that puts the control of the artists back into artist to inspire to be independent. We provide the outlet for Artists' brands to build the way they intended. We are your partner in this journey. Be heard. Be seen. Be independent. </p>
-        <p><a href="#faq">Back to top</a></p>
-        </article>
-        
-        <article id='anchor_how_much'>
-        <p>How much does MyArtistDNA cost?<br />
-        Nothing if you choose us as your partner. We call this the OPT IN option. By opting in we will guide you down this success path and equip you with the tools to succeed. You can OPT OUT. OPT OUT is an option that you pay a monthly fee to use the software and gives you the software with limited features to deploy. </p>
-        <p><a href="#faq">Back to top</a></p>
-        </article>
-        
-        <article id='anchor_already_have_site'>
-        <p>I already have a site, how can I integrate it with MyArtistDNA?<br />
-        We encourage you use MyArtistDNA as your home of your brand. Upload all your content. Use other outlets as your marketing tools. Your account can be, replace or accompany your personal website.</p>
-        <p><a href="#faq">Back to top</a></p>
-        </article>
-        
-        <article id='anchor_max'>
-        <p>What's the maximum amount of content an user can upload?<br />
-       </p>
-        <p><a href="#faq">Back to top</a></p>
-        </article>
-        
-         <article id='anchor_type'>
-        <p>What type and size of files can I upload?<br />
-        </p>
-        <p><a href="#faq">Back to top</a></p>
-        </article>
-        
-         <article id='anchor_download'>
-        <p>Can fans download music?<br />
-       YESS!!!</p>
-        <p><a href="#faq">Back to top</a></p>
-        </article>
-        
-         <article id='anchor_fan_email'>
-        <p>Can I collect fans emails?<br />
-        YESS!!!</p>
-        <p><a href="#faq">Back to top</a></p>
-        </article>
-        
-         <article id='anchor_who'>
-        <p>Who's using MyArtistDNA?<br />
-        Artists and fans. The Artists is our customer. Today we have focused on music and fashion. </p>
-        <p><a href="#faq">Back to top</a></p>
-        </article>
-        
-         <article id='anchor_domain'>
-        <p>Can I point my own domain to MyArtistDNA site? <br />
-        Yes! Or your account can be your website.</p>
-        <p><a href="#faq">Back to top</a></p>
-        </article>
-        
-         <article id='anchor_sell'>
-        <p>Can I sell anything?<br />
-        YES!!! Each account comes with their own store.
+<li><span class="number">1</span><span class="details"><a href="#anchor_what">What is MyArtistDNA?</a></span></li>
+<li><span class="number">2</span><span class="details"><a href="#anchor_how_much">How much does MyArtistDNA cost?</a></span></li>
+<li><span class="number">3</span><span class="details"><a href="#anchor_already_have_site">I already has a site, how can I integrate it with MyArtistDNA?</a></span></li>
+<li><span class="number">4</span><span class="details"><a href="#anchor_max">What's the maximum about of content an user can upload?</a></span></li>
+<li><span class="number">5</span><span class="details"><a href="#anchor_type">What type and size of files can I upload?</a></span></li>
+<li><span class="number">6</span><span class="details"><a href="#anchor_download">Can fans download music?</a></span></li>
+<li><span class="number">7</span><span class="details"><a href="#anchor_fan_email">Can I collect fans emails?</a></span></li>
+<li><span class="number">8</span><span class="details"><a href="#anchor_who">Who's using MyArtistDNA?</a></span></li>
+<li><span class="number">9</span><span class="details"><a href="#anchor_domain">Can I point my own domain to MyArtistDNA site?</a></span></li>
+<li><span class="number">10</span><span class="details"><a href="#anchor_sell">Can I sell anything?</a></span></li>
+<li><span class="number">11</span><span class="details"><a href="#anchor_size">What size images do you suggest to upload?</a></span></li>
+<li><span class="number">12</span><span class="details"><a href="#anchor_band_info">Can I add info about my band?</a></span></li>
+<li><span class="number">13</span><span class="details"><a href="#anchor_social">Can I add my social media?</a></span></li>
+</ul>
+</div>
+
+<div class="checkout">
+<h5>Can't find the answer <br /> you’re looking for?</h5>
+<p>Visit our Help page to contact our support team directly. <a href="tour.php">GO</a></p>
+</div>
+</div>
+
+<div class="subcontent">
+<div class="setlist">
+<article id='anchor_what'>
+<span class="details"><p>What is MyArtistDNA?<br /></span>MyArtistDNA is a ground-breaking platform built uniquely for the arts. A design-driven content management system, branding service and online community, MyArtistDNA enables artists of all types to increase their professional visibility, fan engagement and brand loyalty by sharing their work on one elegant, interactive, centralized hub. We want to help you create the brand you want, and partner with you to get your art out into the world. We want to enable you to be heard, be seen, and be independent.</p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+<article id='anchor_how_much'>
+<p>How much does MyArtistDNA cost?<br />
+Nothing if you choose us as your partner. We call this the OPT IN option. By opting in we will guide you down this success path and equip you with the tools to succeed. You can OPT OUT. OPT OUT is an option that you pay a monthly fee to use the software and gives you the software with limited features to deploy. </p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+<article id='anchor_already_have_site'>
+<p>I already have a site, how can I integrate it with MyArtistDNA?<br />
+We think MyArtistDNA works best if you use it as your main site and the home of your brand, using other outlets like YouTube, Facebook, and Twitter as supplementary marketing tools. Your account can be, replace or accompany your personal website.</p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+<article id='anchor_max'>
+<p>What's the maximum amount of content an user can upload?<br />
+Free accounts - Welcome Package 
+5 videos, 20 songs and 30 images or 1gb of bandwidth. Whichever comes first.</p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+<article id='anchor_type'>
+<p>What type and size of files can I upload?<br />
 </p>
-        <p><a href="#faq">Back to top</a></p>
-        </article>
-        
-         <article id='anchor_size'>
-        <p>What size images do you suggest to upload?<br />
-      </p>
-        <p><a href="#faq">Back to top</a></p>
-        </article>
-        
-         <article id='anchor_band_info'>
-        <p>Can I add info about my band?<br />
-        YES!!! Each account has your own tabs you can make and also we are super believers in METADATA! The more detailed info you can give us the better for YOU!!! You must tag and fingerprint all your work properly so people can find you in unlimited ways!</p>
-        <p><a href="#faq">Back to top</a></p>
-        </article>
+<p><a href="#faq">Back to top</a></p>
+</article>
 
-  <article id='anchor_social'>
-        <p>Can I add my social media?<br />
-       YES!!! We have social media integration.</p>
-        <p><a href="#faq">Back to top</a></p>
-        </article>
-        
-         <article class="last">
-        <p>Is there anyway I can track my stats?<br />
-       YES!!! We have detailed analytics for your account that will tell you a lot of information. See what's popular and control the distribution and order of content based off feedback. 
+<article id='anchor_download'>
+<p>Can fans download music?<br />
+Yes, fans can download music directly from your MyArtistDNA site.  You can set your music for free download, or you can price your tracks and sell them directly through your MyArtistDNA store.  You can also redirect fans who want to purchase tracks to iTunes, Amazon, or other third party retailers.</p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+<article id='anchor_fan_email'>
+<p>Can I collect fans emails?<br />
+You can! As part of the “Socialize” tab on the bottom of your site, fans can input their email address to add themselves to your mailing list as well as we provide notifications through out the fan experience to make sure we collect their information so you can continue to communicate with them.</p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+<article id='anchor_who'>
+<p>Who's using MyArtistDNA?<br />
+Both artists and fans are using MyArtistDNA.  Artists are using it to build sites, upload tracks and videos, share new art, and sell their products.  Fans are visiting these sites, listening to tracks and watching videos, exploring new content, and buying work from their favorite artists.</p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+<article id='anchor_domain'>
+<p>Is there any way I can track my stats?<br />
+Sure! We collect detailed analytics for your account, telling you who’s visiting, from where, and for how long. This can help you see what's popular and what’s not, so can tweak your site to be as effective as possible.</p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+<article id='anchor_domain'>
+<p>What kind of information can I display on my MyArtistDNA site?<br />
+Whatever you want! With MyArtistDNA, you can customize the tabs that are displayed on your site. If you’re in a band that travels a lot, you’ll probably want an “Events” tab to keep your fans in the know about upcoming shows.  If you’re a painter, you may want a “Past Shows” tab that lists your previous exhibitions. It’s up to you!</p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+
+<article id='anchor_domain'>
+<p>How much does MyArtistDNA cost? <br />
+You can start for free. As you build your brand we strongly suggest you upgrade to the launch package which truly get your brand into the real exposure, distribution and monetize space. For pennies a day investment in yourself you will make your brand dramatically stronger. We also can provide branding services to help you get going on the fast track.</p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+<article id='anchor_domain'>
+<p>Can I point my own domain to MyArtistDNA site? <br />
+Yes! If you already have a domain name, you can point it to your MyArtistDNA site.  Or, you can use your MyArtistDNA site as your website.</p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+<article id='anchor_sell'>
+<p>Can I sell art and merchandise through my MyArtistDNA site?<br />
+Yes, you can. You can upgrade your account to get your own store. 
 </p>
-        <p><a href="#faq">Back to top</a></p>
-        </article>
+<p><a href="#faq">Back to top</a></p>
+</article>
 
-        </div>
-        </div>
-    </div><!-- faq -->
-	
+<article id='anchor_size'>
+<p>What size images do you suggest to upload?<br />
+This will vary depending on the users monitor resolution. But we suggest 1200px x 800px, and no larger than 2mbs. </p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+<article id='anchor_band_info'>
+<p>Can I add info about my band?<br />
+YES!!! Each account has your own tabs you can make and also we are super believers in METADATA! The more detailed info you can give us the better for YOU!!! You must tag and fingerprint all your work properly so people can find you in unlimited ways!</p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+<article id='anchor_social'>
+<p>Can I add my social media?<br />
+Absolutely! We are big believers in social media integration. You can sync up your Twitter and Facebook accounts to your MyArtistDNA site, and fans can view and interact with you through the “Socialize” tab at the bottom of your site. </p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+<article class="last">
+<p>Is there anyway I can track my stats?<br />
+YES!!! We have detailed analytics for your account that will tell you a lot of information. See what's popular and control the distribution and order of content based off feedback. 
+</p>
+<p><a href="#faq">Back to top</a></p>
+</article>
+
+</div>
+</div>
+</div><!-- faq -->
+
 
 </section><!-- content -->
 </section><!-- wrapper -->
