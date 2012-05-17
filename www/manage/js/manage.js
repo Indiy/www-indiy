be442274eb749814414ec472fb9c657e0a6ecdae
Jim Lake <jim@blueskylabs.com>
2012-05-17 16:50:10 -0700

    admin user funtions

--- www/manage/js/manage.js
+++ www/manage/js/manage.js
@@ -7,67 +7,6 @@ var EMAIL_REGEX = new RegExp('[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+
 
 var FB_REGEX = new RegExp('http://www.facebook.com/(.*)','i');
 
-
-function onAddUserSubmit()
-{
-    showProgress("Adding user...");
-
-    var artist = $('#artist').val();
-    var url = $('#url').val();
-    var email = $('#email').val();
-    var password = $('#password').val();
-    
-    var post_url = "/manage/add_user.php?";
-    post_url += "&artist=" + escape(artist);
-    post_url += "&url=" + escape(url);
-    post_url += "&email=" + escape(email);
-    post_url += "&password=" + escape(password);
-    jQuery.ajax(
-    {
-        type: 'POST',
-        url: post_url,
-        dataType: 'json',
-        success: function(data) 
-        {
-            showSuccess("User added.");
-        },
-        error: function()
-        {
-            showFailure("Update Failed");
-        }
-    });
-    return false;
-}
-
-function onAddLabelSubmit()
-{
-    showProgress("Adding label...");
-
-    var name = $('#name').val();
-    var email = $('#email').val();
-    var password = $('#password').val();
-    
-    var post_url = "/manage/add_label.php?";
-    post_url += "&name=" + escape(name);
-    post_url += "&email=" + escape(email);
-    post_url += "&password=" + escape(password);
-    jQuery.ajax(
-    {
-        type: 'POST',
-        url: post_url,
-        dataType: 'json',
-        success: function(data) 
-        {
-            showSuccess("Label added.");
-        },
-        error: function()
-        {
-            showFailure("Update Failed");
-        }
-    });
-    return false;
-}
-
 function checkElementFileExtensions(file,extensions,error_string)
 {
     if( file.files && file.files.length > 0 )
---
+++ www/manage/js/user_admin.js
@@ -0,0 +1,63 @@
+
+function onAddUserSubmit()
+{
+    showProgress("Adding user...");
+
+    var artist = $('#artist').val();
+    var url = $('#url').val();
+    var email = $('#email').val();
+    var password = $('#password').val();
+    
+    var post_url = "/manage/data/user_admin.php?";
+    post_url += "&add_user=1";
+    post_url += "&artist=" + escape(artist);
+    post_url += "&url=" + escape(url);
+    post_url += "&email=" + escape(email);
+    post_url += "&password=" + escape(password);
+    jQuery.ajax(
+    {
+        type: 'POST',
+        url: post_url,
+        dataType: 'json',
+        success: function(data) 
+        {
+            showSuccess("User added.");
+        },
+        error: function()
+        {
+            showFailure("Update Failed");
+        }
+    });
+    return false;
+}
+
+function onAddLabelSubmit()
+{
+    showProgress("Adding label...");
+
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var password = $('#password').val();
+    
+    var post_url = "/manage/data/user_admin.php?";
+    post_url += "&add_label=1";
+    post_url += "&name=" + escape(name);
+    post_url += "&email=" + escape(email);
+    post_url += "&password=" + escape(password);
+    jQuery.ajax(
+    {
+        type: 'POST',
+        url: post_url,
+        dataType: 'json',
+        success: function(data) 
+        {
+            showSuccess("Label added.");
+        },
+        error: function()
+        {
+            showFailure("Update Failed");
+        }
+    });
+    return false;
+}
+
