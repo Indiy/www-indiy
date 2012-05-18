5dedc5200d1cf00134f84199d1f4984c5aefc2d3
Jim Lake <jim@blueskylabs.com>
2012-05-18 11:49:18 -0700

    admin user funtions

--- www/manage/js/popup.js
+++ www/manage/js/popup.js
@@ -1,54 +1,25 @@
-// JavaScript Document
 
- 
-$(document).ready(function() {	
- 
-	//select all the a tag with name equal to modal
-	$('a[name=modal]').click(function(e) {
-		//Cancel the link behavior
-		e.preventDefault();
-		
-		//Get the A tag
-		var id = $(this).attr('href');
-	
-		//Get the screen height and width
-		var maskHeight = $(document).height();
-		var maskWidth = $(window).width();
-	
-		//Set heigth and width to mask to fill up the whole screen
-		$('#mask').css({'width':maskWidth,'height':maskHeight});
-		
-		//transition effect		
-		$('#mask').fadeIn(600);	
-		$('#mask').fadeTo("slow",0.5);	
-	
-		//Get the window height and width
-		var winH = $(window).height();
-		var winW = $(window).width();
-              
-		//Set the popup window to center
-		$(id).css('top',  winH/2-$(id).height()/2);
-		$(id).css('left', winW/2-$(id).width()/2);
-	
-		//transition effect
-		$(id).fadeIn(600); 
-	
-	});
-	
-	//if close button is clicked
-	$('.window .close').click(function (e) {
-		//Cancel the link behavior
-		e.preventDefault();
-		
-		$('#mask').hide();
-		$('.window').hide();
-	});		
-	
-	//if mask is clicked
-	$('#mask').click(function () {
-		$(this).hide();
-		$('.window').hide();
-	});			
-	
-});
- 
+var g_popupNumber = 0;
+
+function showPopup(selector,immediate)
+{
+    $('.popup_wrapper').hide();
+    if( immediate )
+    {
+        $(selector).show();
+        $('#mask').show();
+    }
+    else
+    {
+        $(selector).fadeIn();
+        $('#mask').fadeIn();
+    }
+}
+function closePopup()
+{
+    $('.popup_wrapper').fadeOut();
+    $('#mask').fadeOut();
+    g_popupNumber++;
+}
+
+
