4cdac6a548a7605a338ef0bdb554f974a2e7d37c
Jim Lake <jim@blueskylabs.com>
2012-06-01 23:30:14 -0700

    photo admin

--- www/manage/data/photo.php
+++ www/manage/data/photo.php
@@ -28,11 +28,13 @@
         $row = mf(mq("SELECT * FROM photos WHERE id='$photo_id'"));
         
         array_walk($row,cleanup_row_element);
+        
         $image_path = "../artists/photo/" . $row['image'];
-        if( !empty($row['image']) )
-            $row['image'] = $image_path;
+        if( !empty($row['image']) && file_exists($image_path) )
+            $row['image_url'] = $image_path;
         else
-            $row['image'] = "images/photo_video_01.jpg";
+            $row['image_url'] = "images/photo_video_01.jpg";
+
         return $row;
     }
     
