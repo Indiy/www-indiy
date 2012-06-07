cd9db58ed0a221d297815a3c287d3106625dc6d3
Jim Lake <jim@blueskylabs.com>
2012-06-07 10:03:23 -0700

    search

--- www/js/player_search.js
+++ www/js/player_search.js
@@ -141,12 +141,12 @@ function searchRenderResults()
         for( var i = 0 ; i < list.length ; ++i )
         {
             var item = list[i];
-            //html += "<a href='{0}'>".format(item.url);;
+            html += "<a href='{0}'>".format(item.url);;
             html += " <div class='item'>";
             html += "  <img src='{0}'/>".format(item.image);
             html += "  <div class='value'>{0}</div>".format(item.value);
             html += " </div>";
-            //html += "</a>";
+            html += "</a>";
         }
         
         html += "</div>";
