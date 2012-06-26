d20c1d5befb36e64a44ceb6393380b957c903294
Jim Lake <jim@blueskylabs.com>
2012-06-25 19:18:16 -0700

    homepage search

--- www/header.php
+++ www/header.php
@@ -11,7 +11,6 @@
     <meta name="description" CONTENT="MyArtistDNA empowers artists to be heard, be seen & be independent, by giving them the tools, technology & outlet needed to create a powerful web presence." />
     
      <meta name="keywords" content="photographer websites, brand websites, simple websites, band websites, music websites, artist websites, musician websites" />
-    <meta name="google-site-verification" content="dl7cY7ZQDazID-ZKYgntdo_WAr1haMjidTUbNxrGPzo" />
 
     <link href="css/style.css" rel="stylesheet" type="text/css">
     <link rel="icon"  href="favicon.ico" />
@@ -46,11 +45,9 @@
 
 ?>
     
-    <!--SEARCH-->
     <script src="/js/jquery.kwicks-1.5.1.js" type="text/javascript"></script>
-    <script src="/jplayer/js/ra_controls.js" type="text/javascript"></script>
-    <script src="/jplayer/js/index.js" type="text/javascript"></script>
-
+    <script src="/js/search.js" type="text/javascript"></script>
+    
     <script src="/js/login_signup.js" type="text/javascript"></script>
     <script src="/js/footer.js" type="text/javascript"></script>
 </head>
@@ -62,37 +59,30 @@
     <h1><a href="home.php"><img src="images/MYARTISTDNA.gif" alt="MYARTISTDNA"></a></h1>
 
     <nav>
-    <ul>
-   <!--  <li><a href="artists.php">ARTISTS</a>
-		<span>
         <ul>
-        <li><a href="artists.php">All</a></li>
-        <li><a href="music.php">MUSIC</a></li>
-        <li><a href="art.php">ART</a></li>
+            <li>
+                <a href="be-heard.php">BENEFITS</a>
+                <span>
+                    <ul>
+                        <li></li>
+                        <li><a href="be-heard.php">BE HEARD</a></li>
+                        <li><a href="be-seen.php">BE SEEN</a></li>
+                        <li><a href="be-independent.php">BE INDEPENDENT</a></li>
+           
+                    </ul>
+                </span>  
+            </li>
+            <li><a href="artist-kit.php" target="_self">ARTIST KIT</a></li>
+            <li><a href="pricing.php" target="_self">PRICING</a></li>                       
+            <li><a href="tour.php">ABOUT US</a></li>                     
+            <li class="nodivider"><a class="login" href="#" onclick='showLogin();'>LOGIN</a></li>
         </ul>
-		</span>
-    </li> -->                      
-    <li><a href="be-heard.php">BENEFITS</a><span>
-        <ul>
-        <li></li>
-        <li><a href="be-heard.php">BE HEARD</a></li>
-        <li><a href="be-seen.php">BE SEEN</a></li>
-        <li><a href="be-independent.php">BE INDEPENDENT</a></li>
-       
-        </ul>
-		</span>     
-    <li><a href="artist-kit.php" target="_self">ARTIST KIT</a></li>
-    <li><a href="pricing.php" target="_self">PRICING</a></li>                       
-    <li><a href="tour.php">ABOUT US</a></li>                     
-    <li class="nodivider"><a class="login" href="#" onclick='showLogin();'>LOGIN</a></li>
-    </ul>
-    <div id="theSearchBox">  </div>
-    <!--<div class="search">
-    <fieldset>
-    <input name="" value="SEARCH" type="text" class="input" />
-    <input name="" type="image" src="images/icon_search.gif" class="button">
-    </fieldset>
-    </div>-->
+        <div id="search">
+            <div class="search_box">
+                <input />
+            </div>
+            <div id="search_results" class="search_results"></div>
+        </div>
     </nav>
 
 </header>
