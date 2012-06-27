51e9eb3cc48823484b71fde5ac909f35fe0b4608
Jim Lake <jim@blueskylabs.com>
2012-06-27 16:45:08 -0700

    order editor

--- www/manage/js/order.js
+++ www/manage/js/order.js
@@ -11,7 +11,63 @@ function confirmRefund()
 
 function refundOrder()
 {
-    
+    var args = {
+        order_id: g_orderId,
+        method: "refund"
+    };
+
+    jQuery.ajax(
+    {
+        type: 'POST',
+        url: '/manage/data/order.php',
+        data: args,
+        dataType: 'json',
+        success: function(data) 
+        {
+            $('#order .status').html("Canceled");
+        },
+        error: function()
+        {
+            window.alert("Refund failed");
+        }
+    });
 }
 
+function markShipped()
+{
+    var ship_date = $('#tracking_ship_date').val();
+    var tracking_number = $('#tracking_number').val();
+    
+    if( ship_date.length == 0 )
+    {
+        window.alert("Please enter a ship date.");
+        return;
+    }
+    
+    var args = {
+        order_id: g_orderId,
+        method: "ship",
+        ship_date: ship_date
+    };
+    
+    if( tracking_number.length > 0 )
+        args.tracking_number = tracking_number;
+    
+    jQuery.ajax(
+    {
+        type: 'POST',
+        url: '/manage/data/order.php',
+        data: args,
+        dataType: 'json',
+        success: function(data) 
+        {
+            $('#order .status').html("Shipped");
+        },
+        error: function()
+        {
+            window.alert("Adding Shipping Data failed");
+        }
+    });
+    
+}
 
