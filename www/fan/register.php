5bd1c3869120e3b88051d232fab4948ead291111
Jim Lake <jim@blueskylabs.com>
2012-07-05 10:30:14 -0700

    fan updates

--- www/fan/register.php
+++ www/fan/register.php
@@ -5,27 +5,37 @@
     
     $register_token = $_REQUEST['token'];
     
+    if( !$register_token )
+    {
+        unknown_token();
+    }
+    
     $fan = mf(mq("SELECT * FROM fans WHERE register_token='$register_token'"));
     
     if( !fan )
+    {
+        unknown_token();
+    }
+    
+    $fan_email = $fan['email'];
+    
+    include_once 'templates/register.html';
+
+    function unknown_token()
     {
         echo <<<END
 
 <html>
         Unknown Token<br/>
         <form>
-        Enter Token: <input name='token'/><br/>
-        <br/>
-        <input type='submit' value='Submit'/><br/>
+            Enter Token: <input name='token'/><br/>
+            <br/>
+            <input type='submit' value='Submit'/><br/>
         </form>
 </html>
 
 END;
         die();
     }
-    
-    $fan_email = $fan['email'];
-    
-    include_once 'templates/register.html';
 
 ?>
