f2630cf9140b5257d8d5c06160d0e682d0cde42b
Jim Lake <jim@blueskylabs.com>
2012-07-06 11:10:56 -0700

    tooltips

--- www/js/player_tooltips.js
+++ www/js/player_tooltips.js
@@ -7,12 +7,12 @@ function tooltipsOnReady()
 }
 
 var TOOLTIP_OFFSET_MAP = {
-    'PHOTOS': -32,
-    'MUSIC': -30,
-    'VIDEO': -30,
-    'LOVE': -20,
-    'PLAY': -20,
-    'VOLUME': -30
+    LOVE: -12,
+    MUSIC: -9,
+    PHOTOS: -15,
+    PLAY: 2,
+    VIDEO: -11,
+    VOLUME: -15
 };
 
 function showTooltip(event)
