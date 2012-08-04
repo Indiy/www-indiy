52e10f65d3e4824033730d0c318477509d974973
Jim Lake <jim@blueskylabs.com>
2012-08-04 15:27:17 -0700

    admin order editor

--- www/manage/edit_order.php
+++ www/manage/edit_order.php
@@ -9,73 +9,83 @@
 	}
     $order_id = $_REQUEST['order_id'];
     
-    $order = mf(mq("SELECT * FROM orders WHERE id='$order_id'"));
+    $order_data = mf(mq("SELECT * FROM orders WHERE id='$order_id'"));
+    if( !$order_data )
+    {
+        die("Unknown order id");
+    }
     
-    $id = $order['id'];
-    $customer_name = $order['customer_name'];
-    $customer_email = $order['customer_email'];
+    $order_json = json_encode($order_data);
     
-    $shippping_amount = $order['shipping_amount'];
-    $charge_amount = $order['charge_amount'];
-    $shipping_info = json_decode($order['shipping_json'],TRUE);
-    $payment_info = json_decode($order['payment_json'],TRUE);
-    $order_state = $order['state'];
-    $ship_date = $order['ship_date'];
-    if( !$ship_date )
-        $ship_date = "None";
+    $shipping_amount = $order_data['shipping_amount'];
+    $charge_amount = $order_data['charge_amount'];
+    $shipping_info = json_decode($order_data['shipping_json'],TRUE);
+    $payment_info = json_decode($order_data['payment_json'],TRUE);
     
-    if( $order_state == 'PENDING_CONFIRM' )
+    $order_email = $order_data['customer_email'];
+    $order_date = strftime("%m-%d-%Y",strtotime($order_data['order_date']));
+    
+    if( $order_data['state'] == 'PENDING_CONFIRM' )
         $order_status = "Waiting For Customer Confirmation";
-    else if( $order_state == 'PENDING_PAYMENT' )
+    else if( $order_data['state'] == 'PENDING_PAYMENT' )
         $order_status = "Payment Processing Pending";
-    else if( $order_state == 'PENDING_SHIPMENT' )
+    else if( $order_data['state'] == 'PENDING_SHIPMENT' )
         $order_status = "Waiting For Shipment";
-    else if( $order_state == 'SHIPPED' )
+    else if( $order_data['state'] == 'SHIPPED' )
         $order_status = "Shipped";
-    else if( $order_state == 'CLOSED' )
-        $order_status = "Closed";
-    else if( $order_state == 'CANCELED' )
+    else if( $order_data['state'] == 'CLOSED' )
+        $order_status = "Complete";
+    else if( $order_data['state'] == 'CANCELED' )
         $order_status = "Canceled";
-    else if( $order_state == 'ABANDONED' )
+    else if( $order_data['state'] == 'ABANDONED' )
         $order_status = "Order Abandoned";
     else
         $order_status = "Unknown";
-        
+    
     $order_items = array();
     $order_item_html = "";
+    $order_items = store_get_order($order_id);
     
-    $q_order_items = mq("SELECT * FROM order_items WHERE order_id='$order_id'");
-    $i = 0;
-    while( $item = mf($q_order_items) )
+    foreach( $order_items as $i => $item )
     {
+        $name = $item['name'];
         $description = $item['description'];
         $color = $item['color'];
         $size = $item['size'];
         $price = $item['price'];
         $quantity = $item['quantity'];
-        $order_item = array("quantity" => $quantity,
-                            "description" => $description,
-                            "product_id" => $item['product_id'],
-                            "color" => $color,
-                            "size" => $size);
-                            
-        $order_items[] = $order_item;
+        $image = $item['image'];
+        $type = $item['type'];
         
         $num = $i + 1;
+        
+        if( $i % 2 == 0 )
+            $odd = " odd";
+        else
+            $odd = "";
+        
         $html = "";
-        $html .= "<div class='item'>";
-        $html .= " <div class='num'>$num</div>";
-        $html .= " <div class='description'>$description</div>";
-        $html .= " <div class='price'>$price</div>";
+        $html .= "<div class='item$odd'>";
         $html .= " <div class='quantity'>$quantity</div>";
+        $html .= " <div class='image'>";
+        $html .= "  <div class='image_holder'><img src='$image'></div>";
+        $html .= " </div>";
+        $html .= " <div class='name_description wide'>";
+        $html .= "  <div class='name'>$name</div>";
+        $html .= "  <div class='description'>$description</div>";
+        $html .= " </div>";
+        $html .= " <div class='price'>\$$price</div>";
         $html .= "</div>";
         
         $order_item_html .= $html;
-        
-        $i++;
     }
-
-        
+    
+    $tracking_number = FALSE;
+    if( isset( $shipping_info['tracking_number'] ) )
+    {
+        $tracking_number = $shipping_info['tracking_number'];
+    }
+    
     $include_order = TRUE;
     $include_editor = FALSE;
     
