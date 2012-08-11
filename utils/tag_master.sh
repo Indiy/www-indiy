9dbab301441a7aaea8c8f6b32d9642b4b07be4d5
Jim Lake <jim@blueskylabs.com>
2012-08-11 01:26:25 -0700

    git manipulation scripts

--- utils/tag_master.sh
+++ utils/tag_master.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-if [ -z "$@" ]
+DATE=$(date +"%Y%m%d_%H%M%S")
+TAG="MASTER_TAG_$DATE"
+COMMENT="$@"
+
+if [ -z "$COMMENT" ]
  then
   echo "Please supply a commit comment for the tag"
   exit -1
 fi
 
-DATE=$(date +"%Y%m%d_%H%M%S")
-TAG="MASTER_TAG_$DATE"
-COMMENT="$@"
 
 pushd ~/sandbox/MAD >/dev/null
 
 echo "Tagging with tag: $TAG"
-git tag -a $TAG -m "$COMMENT"
-git push --tags
+echo git tag -a $TAG -m "$COMMENT"
+echo git push --tags
 echo "Tagged and pushed!"
