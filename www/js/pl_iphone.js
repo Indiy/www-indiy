de598d4de278e0057704693b299fc2327ee16d0a
Jim Lake <jim@blueskylabs.com>
2012-08-22 09:37:58 -0700

    iphone site

--- www/js/pl_iphone.js
+++ www/js/pl_iphone.js
@@ -2,8 +2,13 @@
 
 function iphoneGeneralReady()
 {
-    
+    scrollToTop();
+    $(window).resize(scrollToTop);
 }
 $(document).ready(iphoneGeneralReady);
 
+function scrollToTop()
+{
+    window.scrollTo(0,1);
+}
 
