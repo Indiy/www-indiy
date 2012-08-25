6e3d54c2ee233e85ee926adeeec5c359b16dadd3
Jim Lake <jim@blueskylabs.com>
2012-08-24 23:22:51 -0700

    analytics updates

--- www/manage/stats.php
+++ www/manage/stats.php
@@ -13,33 +13,6 @@
 	$id = $_REQUEST["userId"];
 	
 	$total_player_views = artist_get_total_views($id);
-	
-	// Build Page Graph
-	$loadpages = mq("select `views` from `[p]musicplayer_content` where `artistid`='{$id}' order by `order` asc, `id` desc");
-	while ($pa = mf($loadpages)) {
-		$buildmax .= $pa["views"].",";
-	}
-	$size = 100 / max(num($loadpages),1);
-	//$size = num($loadpages) / 100;
-	$buildmax = explode(",", $buildmax);
-	$max = max($buildmax);
-    if( $max == 0 )
-        $max = 1;
-
-	$loadpag = mq("select `id`,`name`,`artistid`,`order`,`views` from `[p]musicplayer_content` where `artistid`='{$id}' order by `order` asc, `id` desc");
-	while ($pages = mf($loadpag)) {
-		$page_id = $pages["id"];
-		$page_views = $pages["views"];
-		$page_percent = floor(($page_views / $max) * 100);
-		$page_filler = floor(100 - $page_percent);
-		$page_name = stripslashes($pages["name"]);
-		$pageList .= '
-			<div class="bar page" style="width: '.$size.'%; background-position: 0px '.$page_filler.'px !important;">'.$page_views.'</div>
-		';
-		$pageNames .= '
-			<div style="width: '.$size.'%; float: left; text-align: center;">'.$page_name.'</div>
-		';
-	}
 
     function make_q_html($sql,$class='')
     {
@@ -122,6 +95,9 @@
 
     $sql = "SELECT id,name,loves AS views FROM photos WHERE artist_id='$id' ORDER BY `order` ASC, `id` DESC";
     $photo_loves_html = make_q_html($sql);
+
+    $sql = "SELECT id,name,downloads AS views FROM mydna_musicplayer_audio WHERE artistid='$id' AND downloads > 0 ORDER BY `order` ASC, `id` DESC";
+    $song_downloads_html = make_q_html($sql);
 
 
     $include_order = FALSE;
