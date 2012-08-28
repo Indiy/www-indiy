29dc8e0b924775a7cb6d37f51c04d540b7bcd009
Jim Lake <jim@blueskylabs.com>
2012-08-28 16:17:35 -0700

    bulk uploads

--- www/manage/js/edit_tab.js
+++ www/manage/js/edit_tab.js
@@ -21,7 +21,6 @@ function showTabPopup(tab_index)
     g_removeTabImage = false;
     
     $('#edit_tab #artist_id').val(g_artistId);
-    clearFileElement('#edit_tab #content_image');
     
     if( tab_index !== false )
     {
@@ -31,16 +30,8 @@ function showTabPopup(tab_index)
         $('#edit_tab #name').val(tab.name);
         var body = getEncodedBody(tab.body)
         g_editor.setEditorHTML(body);
-        if( tab.image )
-        {
-            var html = "<img src='{0}' />".format(tab.image_url);
-            html += "<button onclick='return onTabImageRemove();'></button>";
-            $('#edit_tab .image_image').html(html);
-        }
-        else
-        {
-            $('#edit_tab .image_image').empty();
-        }
+        
+        fillArtistFileSelect('#edit_tab #image_drop','IMAGE',tab.image);
     }
     else
     {
@@ -53,7 +44,8 @@ function showTabPopup(tab_index)
         $('#edit_tab #content_id').val("");
         $('#edit_tab #name').val("");
         g_editor.setEditorHTML("");
-        $('#edit_tab .image_image').empty();
+        
+        fillArtistFileSelect('#edit_tab #image_drop','IMAGE',false);
     }
     showPopup('#edit_tab');
     return false;
@@ -82,18 +74,16 @@ function onAddContentSubmit()
         var artist_id = $('#edit_tab #artist_id').val();
         var content_id = $('#edit_tab #content_id').val();
         var name = $('#edit_tab #name').val();
+        var image_drop = $('#edit_tab #image_drop').val();
         
         form_data.append('artistid',artist_id);
         form_data.append('id',content_id);
         form_data.append('name',name);
         form_data.append('body',body);
         form_data.append('remove_image',g_removeTabImage);
+
+        form_data.append('image_drop',image_drop);
         
-        var content_image = $('#edit_tab #content_image')[0];
-        if( content_image.files && content_image.files.length > 0 )
-        {
-            form_data.append('logo',content_image.files[0]);
-        }
         form_data.append('submit','submit');
         form_data.append('ajax',true);
     }
