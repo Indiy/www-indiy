2f3458b63712c1a958a2aebad07ab5a82dda9e43
Jim Lake <jim@blueskylabs.com>
2012-09-02 18:37:01 -0700

    fan header

--- www/manage/index.php
+++ www/manage/index.php
@@ -3,14 +3,22 @@
 	require_once '../includes/config.php';
 	include_once '../includes/functions.php';	
 
-	if($_SESSION['sess_userId'] == '')
+	if( $_SESSION['sess_userId'] == '' )
 	{
 		header("Location: /index.php");
 		exit();
 	}
     else
     {
-        header("Location: dashboard.php");
+        if( $_SESSION['sess_userType'] == 'ARTIST' )
+        {
+            $artist_id = $_SESSION['sess_userId'];
+            header("Location: /manage/artist_management.php?userId=$artist_id");
+        }
+        else
+        {
+            header("Location: /manage/dashboard.php");
+        }
     }
 
 ?>
