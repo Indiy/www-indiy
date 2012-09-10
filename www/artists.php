24ee6ec1af06d751a35af7a396fef3a21a004a2d
Jim Lake <jim@blueskylabs.com>
2012-09-10 09:31:50 -0700

    all artist pages

--- www/artists.php
+++ www/artists.php
@@ -1,97 +1,50 @@
 <?php
-include "header.php";
+
+    require_once 'includes/config.php';
+    require_once 'includes/functions.php';
+    
+    $list_html = "";
+    
+    $artists_q = mq("SELECT * FROM mydna_musicplayer WHERE preview_key = ''");
+    while( $artist = mf($artist_q) )
+    {
+        $logo = $artist['logo'];
+        $logo_url = "/artists/files/$logo";
+        $name = $artist['artist'];
+        
+        $html = "";
+        $html .= "<li>";
+        $html .= " <img src='$logo_url' />";
+        $html .= " <span>$name</span>";
+        $html .= "</li>";
+        
+        $list_html .= $html;
+    }
+
+    include_once "header.php";
 ?>
 
 <section id="wrapper">
 <section id="content">
 	
 	<div id="artists">
-	<div class="heading">
-    <h2>ALL ARTISTS</h2>
-    <!--
-    <div class="sort">
-        <ul class="splitter">
-        <li><a class="sub" href="#">SORT PROJECTS BY CATEGORY
-        	<ul class="sublist">
-            <li class="segment-0"><a href="#" data-value="all">All</li>
-            <li class="segment-1"><a href="#" data-value="rock">Rock</li>
-            <li class="segment-2"><a href="#" data-value="hiphop">Hip Hop</li>
-            <li class="segment-2"><a href="#" data-value="pop">Pop</li>
-            <li class="segment-2"><a href="#" data-value="dance">Dance</li>
-            <li class="segment-1"><a href="#" data-value="country">Country</li>
-            </ul>
-        </li>
-        </ul>
-      </div>  dropdown -->
-    </div>
+        <div class="heading">
+            <h2>ALL ARTISTS</h2>
+        </div>
 	
-    <div id="artistshome">
-    	<ul id="list" class="image-grid">
-        <li data-id="id-1" ><img src="images/bv.jpg" alt="">
-        <span>Black Violin</span></li>
-        
-        <li data-id="id-2" ><img src="images/coolMent.jpg" alt="">
-        <span>Cool Mentality</span></li>
-        
-        <li data-id="id-3" ><img src="images/wondaland.jpg" alt="">
-        <span>Woodlands Arts Society</span></li>
-        
-        <li data-id="id-4" ><img src="images/artists_04.jpg" alt="">
-        <span>Kithe Brewster</span></li>
-        
-        <li data-id="id-5" ><img src="images/bc.jpg" alt="">
-        <span>Babylon Cartel</span></li>
-        
-        <li data-id="id-6" ><img src="images/artists_03.jpg" alt="">
-        <span>Hireosonic</span></li>
-		
-		<li data-id="id-7" ><img src="images/nvg.jpg" alt="">
-        <span>Navegante</span></li>
-        
-        <li data-id="id-8" ><img src="images/youngc.jpg" alt="">
-        <span>Young Chris</span></li>
-        
-        <li data-id="id-9" ><img src="images/marc.jpg" alt="">
-        <span>Marc Ecko</span></li>
-        
-        <li data-id="id-10" ><img src="images/blakroc.jpg" alt="">
-        <span>Blakroc</span></li>
-        
-        <li data-id="id-11" ><img src="images/mmjb.jpg" alt="">
-        <span>Money Making Jam Boys</span></li>
-                
-        <li><img src="images/jen.jpg" alt="">
-        <span>Jen</span></li>
-        
-        <li><img src="images/rbanks.jpg" alt="">
-        <span>Ryan Bank$</span></li>
- 
-        <li><img src="images/artists_05.jpg" alt="">
-        <span>Vanessa Cantave</span></li>
-        
-        <li><img src="images/hssd-sm.jpg" alt="">
-        <span>Heart Set Self Destruct</span></li>
-        
-        <li><img src="images/basil.jpg" alt="">
-        <span>Basil</span></li>
-        
-        <li><img src="images/onionflats.jpg" alt="">
-        <span>Onion Flats</span></li>
-
-        
-        </ul>
-       
-	</div>	
-    </div><!-- artists -->
+        <div id="artistshome">
+            <ul id="list" class="image-grid">
+                <?=$list_html;?>
+            </ul>
+        </div>
+    </div>
     
-      <div class="signup">
-    <h3>Be heard, be seen, Get started now!</h3>
-    <div class="button"><a href="#" onclick="showSignup();">SIGN UP NOW</a></div>
-    </div><!-- signup -->
+    <div class="signup">
+        <h3>Be heard, be seen, Get started now!</h3>
+        <div class="button"><a href="#" onclick="showSignup();">SIGN UP NOW</a></div>
+    </div>
 
-</section><!-- content -->
-</section><!-- wrapper -->
+</section>
+</section>
 
-<?php
-include "footer.php";
-?>
+<?php include_once "footer.php"; ?>
