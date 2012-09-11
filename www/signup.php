3817c6599c23566551011bde4c4d693f7fe77228
Jim Lake <jim@blueskylabs.com>
2012-09-11 14:14:38 -0700

    signup

--- www/signup.php
+++ www/signup.php
@@ -1,22 +1,7 @@
-<?php include_once "header.php"; ?>
+<?php
 
-<section id="wrapper">
-<section id="content">
 
-    <a href="/fan">Signup for a Fan Account</a><br/>
-    <br/>
-    <a href="#" onclick="showSignup();">Signup for an Artist Account</a><br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-</section>
-</section>
-        
-<?php include_once "footer.php"; ?>
+
+
+    include_once 'templates/signup.html';
+?>
