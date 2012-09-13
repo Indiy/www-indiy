88494f39fdcdaf73e1df95c177f3f946227386db
Jim Lake <jim@blueskylabs.com>
2012-09-12 18:40:25 -0700

    signup

--- www/js/signup.js
+++ www/js/signup.js
@@ -4,12 +4,12 @@ function signupCheckBox(signup_type)
     if( signup_type == 'fan' )
     {
         $('#signup .check_item.fan input').attr('checked','checked');
-        $('#signup .check_item.artist input').attr('checked','');
+        $('#signup .check_item.artist input').removeAttr('checked');
         $('#signup #artist_items').hide();
     }
     else
     {
-        $('#signup .check_item.fan input').attr('checked','');
+        $('#signup .check_item.fan input').removeAttr('checked');
         $('#signup .check_item.artist input').attr('checked','checked');
         $('#signup #artist_items').show();
     }
