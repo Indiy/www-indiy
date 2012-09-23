fb9e713d0dcccaaea2c32265c175ffa01ed15e6c
Jim Lake <jim@blueskylabs.com>
2012-09-23 11:01:28 -0700

    signup

--- www/manage/header.php
+++ www/manage/header.php
@@ -82,7 +82,7 @@
 </div>
 <section id="bgtopbar">
 	<section id="topbar">
-		<p>Logged in as <?php echo $_SESSION['sess_userName']; ?> | <a href="/logout.php">Logout</a> | <a href="/logout.php">Help</a> </p>
+		<p>Logged in as <?php echo $_SESSION['sess_userName']; ?> | <a href="/logout.php">Logout</a> | <a href="#">Help</a> </p>
 	</section>
 </section>
 <section id="headerinner">
