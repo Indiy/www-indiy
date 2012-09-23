50aa783c6f677eeca970cc288eda604bea9c35f4
Jim Lake <jim@blueskylabs.com>
2012-09-23 10:48:03 -0700

    signup

--- www/Login_Twitbook/signup_facebook.php
+++ www/Login_Twitbook/signup_facebook.php
@@ -40,7 +40,7 @@
         $url = $_SESSION['signup_url'];
         
         $values = array("artist" => $name,
-                        "url" => $usl,
+                        "url" => $url,
                         "facebook" => $fb_name,
                         "fb_uid" => $uid,
                         "fb_access_token" => $fb_access_token,
