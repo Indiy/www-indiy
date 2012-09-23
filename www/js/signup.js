b83324bb26202f8b5b55cc3508a6efe732127ae9
Jim Lake <jim@blueskylabs.com>
2012-09-22 19:24:36 -0700

    signup

--- www/js/signup.js
+++ www/js/signup.js
@@ -1,4 +1,9 @@
 
+
+var HOSTNAME_REGEX = new RegExp('^(?=.{1,255}$)[0-9A-Za-z](?:(?:[0-9A-Za-z]|\\b-){0,61}[0-9A-Za-z])?(?:[0-9A-Za-z](?:(?:[0-9A-Za-z]|\\b-){0,61}[0-9A-Za-z])?)*$');
+var EMAIL_REGEX = new RegExp('[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?');
+
+
 function signupCheckBox(signup_type)
 {
     if( signup_type == 'fan' )
@@ -33,3 +38,50 @@ function signupBlurInput(input,default_text)
     }
 }
 
+function signupSubmit()
+{
+    if( $('#signup .check_item.fan input').is(':checked') )
+    {
+        signupFan();
+    }
+    else
+    {
+        signupArtist();
+    }
+}
+
+function signupArtist()
+{
+    var name = $('#signup #artist_items .site_name input').val();
+    var url = $('#signup #artist_items .site_link input').val();
+    var email = $('#signup .credentials .email input').val();
+    var password = $('#signup .credentials .password input').val();
+    
+    if( name.length == 0 )
+    {
+        window.alert("Please enter a name for your site.");
+        return;
+    }
+    if( url.length == 0 )
+    {
+        window.alert("Please enter a URL for your site.");
+        return;
+    }
+    if( !url.match(HOSTNAME_REGEX) )
+    {
+        window.alert("Please enter a valid URL.  A-Z, a-z, -, 0-9 are allowed.");
+        return;
+    }
+    if( email.length == 0 || !email.match(EMAIL_REGEX) )
+    {
+        window.alert("Please enter a valid email address.");
+        return;
+    }
+    if( password.length == 0 )
+    {
+        window.alert("Please enter a password for your account.");
+        return;
+    }
+    
+    window.alert("Good form");
+}
