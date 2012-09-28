c4c92d8b056d36cffe1a79fa23bb277474340819
Jim Lake <jim@blueskylabs.com>
2012-09-28 13:08:02 -0700

    artist invoices

--- www/manage/js/order.js
+++ www/manage/js/order.js
@@ -92,7 +92,7 @@ function renderArtistStatement()
         var html = "";
         html += "<div class='item{0}' onclick='showInvoice({1});'>".format(odd,i);
         html += " <div class='invoice_id'>{0}</div>".format(invoice_id);
-        html += " <div class='invoice_date'>{0}</div>".format(invoice.order_date);
+        html += " <div class='invoice_date'>{0}</div>".format(invoice.invoice_date);
         html += " <div class='payout_total'>${0}</div>".format(invoice.amount.toFixed(2));
         if( invoice.paid_amount > 0.0 )
         {
