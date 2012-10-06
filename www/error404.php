5c0c00f3fb2993afb562c9ba415183bc86d2beaf
Jim Lake <jim@blueskylabs.com>
2012-10-05 19:56:04 -0700

    maintainence warning

--- www/error404.php
+++ www/error404.php
@@ -1,5 +1,8 @@
 <?php
 
+    header("Cache-Control: no-cache");
+    header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
+
     include_once "includes/config.php";
     include_once "includes/functions.php";
 
--- www/error_maintenance.php
+++ www/error_maintenance.php
@@ -1,6 +1,7 @@
 <?php
 
-    
+    header("Cache-Control: no-cache");
+    header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
     
     if( $_COOKIE['maintenance_cookie'] == 'adivyy129741jblad9y71' )
     {
@@ -25,6 +26,32 @@
     {
         $request_uri = $_SERVER['REQUEST_URI'];
         
-        include ".$request_uri";
+        if( endsWith($request_uri,"php") )
+        {
+            include_once ".$request_uri";
+        }
+        else
+        {
+            $path = ".$request_uri";
+
+            $mime_type = mime_content_type($path);
+            header("Content-Type: $mime_type");
+            
+            $real_path = realpath($path);
+            
+            $length = filesize($real_path);
+            header("Content-Length: $length");
+            
+            readfile($real_path);
+        }
+    }
+    function endsWith($haystack, $needle)
+    {
+        $length = strlen($needle);
+        if ($length == 0) {
+            return true;
+        }
+        
+        return (substr($haystack, -$length) === $needle);
     }
 ?>
