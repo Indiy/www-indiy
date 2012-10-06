b0e1d7dde85f9d0dd8c45e8e5c4bb5f3c410cedc
Alphonso Morris <amorris@levasent.com>
2012-10-06 17:32:35 -0400

    Revert "js edit"
    This reverts commit ed8d7f0ee1b450251da56b7e1e9b333504305596.

--- www/js/home_data.js
+++ www/js/home_data.js
@@ -39,7 +39,7 @@ var g_backgroundList = [
         content_info_html:
             "<div class='logo'></div><a href='http://spikejordan.myartistdna.com' target='_blank'>Spike Jordan<div class='slashes'>//</div>Filmmaker, Philadelphia</a>"        
     },
- {
+    {
         image: "/images/home_bg05.jpg",
         bg_color: "000000",
         bg_style: "STRETCH",
@@ -47,6 +47,8 @@ var g_backgroundList = [
         loaded: false,
         image_data: { width:1267, height:800 },
         content_info_html:
-            "<div class='logo'></div><a href='http://denmbar.myartistdna.com' target='_blank'>Den.m Bar<div class='slashes'>//</div>Clothing Line, Los Angeles</a>"        
+            "<div class='logo'></div><a href='http://spikejordan.myartistdna.com' target='_blank'>Den.m Bar<div class='slashes'>//</div>Clothing Line, Los Angeles</a>"        
     }
-];
+
+]
+;
