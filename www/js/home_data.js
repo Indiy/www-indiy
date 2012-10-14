e4b6b5caa243e744e0e95063ee2ef7a1ac93acf7
Alphonso Morris <amorris@levasent.com>
2012-10-14 16:57:02 -0400

    homepage edits

--- www/js/home_data.js
+++ www/js/home_data.js
@@ -7,17 +7,17 @@ var g_backgroundList = [
         loaded: false,
         image_data: { width:1267, height:800 },
         content_info_html:
-            "<div class='logo'></div><a href='http://erictwillams.myartistdna.com' target='_blank'>Eric T. Willams<div class='slashes'>//</div>Photographer, Philadelphia"
+            "<div class='logo'></div><a href='http://erictwilliams.myartistdna.com' target='_blank'>Eric T. Willams<div class='slashes'>//</div>Photographer, Philadelphia</a>"
     },
-    {
-        image: "/images/home_bg02.jpg",
+    { image: "/images/home_bg05.jpg",
         bg_color: "000000",
         bg_style: "STRETCH",
         bg_justify: "TOP",
         loaded: false,
         image_data: { width:1267, height:800 },
         content_info_html:
-            "<div class='logo'></div><a href='http://pozzie9.myartistdna.com' target='_blank'>Pozzie Mazerati<div class='slashes'>//</div>Music Artist, Philadelphia"
+            "<div class='logo'></div><a href='http://denmbar.myartistdna.com' target='_blank'>Den.m Bar<div class='slashes'>//</div>Clothing Line, Los Angeles</a>"      
+       
     },
     
     {
@@ -31,13 +31,13 @@ var g_backgroundList = [
             "<div class='logo'></div><a href='http://spikejordan.myartistdna.com' target='_blank'>Spike Jordan<div class='slashes'>//</div>Filmmaker, Philadelphia</a>"        
     },
     {
-        image: "/images/home_bg05.jpg",
+       image: "/images/home_bg02.jpg",
         bg_color: "000000",
         bg_style: "STRETCH",
         bg_justify: "TOP",
         loaded: false,
         image_data: { width:1267, height:800 },
         content_info_html:
-            "<div class='logo'></div><a href='http://denmbar.myartistdna.com' target='_blank'>Den.m Bar<div class='slashes'>//</div>Clothing Line, Los Angeles</a>"        
+            "<div class='logo'></div><a href='http://pozzie9.myartistdna.com' target='_blank'>Pozzie Mazerati<div class='slashes'>//</div>Music Artist, Philadelphia</a>"
     }
 ];
