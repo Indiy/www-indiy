13c459d03531dbf5ff662bc5a2008afccf017a25
Alphonso Morris <amorris@levasent.com>
2012-10-14 20:55:24 -0400

    homepage update

--- www/js/home_data.js
+++ www/js/home_data.js
@@ -9,6 +9,16 @@ var g_backgroundList = [
         content_info_html:
             "<div class='logo'></div><a href='http://erictwilliams.myartistdna.com' target='_blank'>ERIC WILLIAMS<div class='slashes'>//</div>PHOTOGRAPHER</a>"
     },
+    {
+       image: "/images/home_bg02.jpg",
+        bg_color: "000000",
+        bg_style: "STRETCH",
+        bg_justify: "TOP",
+        loaded: false,
+        image_data: { width:1267, height:800 },
+        content_info_html:
+            "<div class='logo'></div><a href='http://pozzie9.myartistdna.com' target='_blank'>POZZIE MAZERATI<div class='slashes'>//</div>MUSIC ARTIST</a>"
+    },
     { image: "/images/home_bg05.jpg",
         bg_color: "000000",
         bg_style: "STRETCH",
@@ -21,6 +31,16 @@ var g_backgroundList = [
     },
     
     {
+        image: "/images/home_bg07.jpg",
+        bg_color: "000000",
+        bg_style: "STRETCH",
+        bg_justify: "TOP",
+        loaded: false,
+        image_data: { width:1267, height:800 },
+        content_info_html:
+            "<div class='logo'></div><a href='http://nanni.myartistdna.com' target='_blank'>NANNI<div class='slashes'>//</div>MUSIC ARTIST</a>"        
+    },
+    {
         image: "/images/home_bg04.jpg",
         bg_color: "000000",
         bg_style: "STRETCH",
@@ -29,15 +49,6 @@ var g_backgroundList = [
         image_data: { width:1267, height:800 },
         content_info_html:
             "<div class='logo'></div><a href='http://spikejordan.myartistdna.com' target='_blank'>SPIKE JORDAN<div class='slashes'>//</div>FILMMAKER</a>"        
-    },
-    {
-       image: "/images/home_bg02.jpg",
-        bg_color: "000000",
-        bg_style: "STRETCH",
-        bg_justify: "TOP",
-        loaded: false,
-        image_data: { width:1267, height:800 },
-        content_info_html:
-            "<div class='logo'></div><a href='http://pozzie9.myartistdna.com' target='_blank'>POZZIE MAZERATI<div class='slashes'>//</div>MUSIC ARTIST</a>"
     }
-];
+    
+ ];
