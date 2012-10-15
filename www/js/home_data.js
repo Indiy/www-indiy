b264e45fd234f80cb06fc92c1c490a5090458c9a
Alphonso Morris <amorris@levasent.com>
2012-10-14 21:08:16 -0400

    homepage edit

--- www/js/home_data.js
+++ www/js/home_data.js
@@ -7,9 +7,10 @@ var g_backgroundList = [
         loaded: false,
         image_data: { width:1267, height:800 },
         content_info_html:
-            "<div class='logo'></div><a href='http://pozzie9.myartistdna.com' target='_blank'>POZZIE MAZERATI<div class='slashes'>//</div>MUSIC ARTIST</a>"
+        "<div class='logo'></div><a href='http://pozzie9.myartistdna.com' target='_blank'>POZZIE MAZERATI<div class='slashes'>//</div>MUSIC ARTIST</a>"
     },
-        {
+  
+     {
         image: "/images/home_bg01.jpg",
         bg_color: "000000",
         bg_style: "STRETCH",
@@ -17,8 +18,9 @@ var g_backgroundList = [
         loaded: false,
         image_data: { width:1267, height:800 },
         content_info_html:
-            "<div class='logo'></div><a href='http://erictwilliams.myartistdna.com' target='_blank'>ERIC WILLIAMS<div class='slashes'>//</div>PHOTOGRAPHER</a>"
+      "<div class='logo'></div><a href='http://erictwilliams.myartistdna.com' target='_blank'>ERIC WILLIAMS<div class='slashes'>//</div>PHOTOGRAPHER</a>"
     },
+    
     {	
     	image: "/images/home_bg05.jpg",
         bg_color: "000000",
@@ -29,7 +31,8 @@ var g_backgroundList = [
         content_info_html:
             "<div class='logo'></div><a href='http://denmbar.myartistdna.com' target='_blank'>DEN.M BAR<div class='slashes'>//</div>CLOTHING LINE</a>"      
        
-    }
+    },
+    
     {
         image: "/images/home_bg04.jpg",
         bg_color: "000000",
