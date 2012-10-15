e5374c4d24ad92d0014411b0af379c6d90019ebc
Alphonso Morris <amorris@levasent.com>
2012-10-14 21:04:44 -0400

    img update

--- www/js/home_data.js
+++ www/js/home_data.js
@@ -1,15 +1,5 @@
 var g_backgroundList = [
     {
-        image: "/images/home_bg01.jpg",
-        bg_color: "000000",
-        bg_style: "STRETCH",
-        bg_justify: "CENTER",
-        loaded: false,
-        image_data: { width:1267, height:800 },
-        content_info_html:
-            "<div class='logo'></div><a href='http://erictwilliams.myartistdna.com' target='_blank'>ERIC WILLIAMS<div class='slashes'>//</div>PHOTOGRAPHER</a>"
-    },
-    {
        image: "/images/home_bg02.jpg",
         bg_color: "000000",
         bg_style: "STRETCH",
@@ -19,7 +9,18 @@ var g_backgroundList = [
         content_info_html:
             "<div class='logo'></div><a href='http://pozzie9.myartistdna.com' target='_blank'>POZZIE MAZERATI<div class='slashes'>//</div>MUSIC ARTIST</a>"
     },
-    { image: "/images/home_bg05.jpg",
+        {
+        image: "/images/home_bg01.jpg",
+        bg_color: "000000",
+        bg_style: "STRETCH",
+        bg_justify: "CENTER",
+        loaded: false,
+        image_data: { width:1267, height:800 },
+        content_info_html:
+            "<div class='logo'></div><a href='http://erictwilliams.myartistdna.com' target='_blank'>ERIC WILLIAMS<div class='slashes'>//</div>PHOTOGRAPHER</a>"
+    },
+    {	
+    	image: "/images/home_bg05.jpg",
         bg_color: "000000",
         bg_style: "STRETCH",
         bg_justify: "TOP",
@@ -28,6 +29,16 @@ var g_backgroundList = [
         content_info_html:
             "<div class='logo'></div><a href='http://denmbar.myartistdna.com' target='_blank'>DEN.M BAR<div class='slashes'>//</div>CLOTHING LINE</a>"      
        
+    }
+    {
+        image: "/images/home_bg04.jpg",
+        bg_color: "000000",
+        bg_style: "STRETCH",
+        bg_justify: "TOP",
+        loaded: false,
+        image_data: { width:1267, height:800 },
+        content_info_html:
+            "<div class='logo'></div><a href='http://spikejordan.myartistdna.com' target='_blank'>SPIKE JORDAN<div class='slashes'>//</div>FILMMAKER</a>"        
     },
     
     {
@@ -39,16 +50,5 @@ var g_backgroundList = [
         image_data: { width:1267, height:800 },
         content_info_html:
             "<div class='logo'></div><a href='http://nanai.myartistdna.com' target='_blank'>NANNI<div class='slashes'>//</div>MUSIC ARTIST</a>"        
-    },
-    {
-        image: "/images/home_bg04.jpg",
-        bg_color: "000000",
-        bg_style: "STRETCH",
-        bg_justify: "TOP",
-        loaded: false,
-        image_data: { width:1267, height:800 },
-        content_info_html:
-            "<div class='logo'></div><a href='http://spikejordan.myartistdna.com' target='_blank'>SPIKE JORDAN<div class='slashes'>//</div>FILMMAKER</a>"        
     }
-    
- ];
+];
