18dcd983f8d54517a9658c4e88e3d98d04f61a00
Jim Lake <jim@blueskylabs.com>
2012-11-19 14:32:08 -0800

    fm updates for fonz

--- rocnation.fm/js/madfm.js
+++ rocnation.fm/js/madfm.js
@@ -66,7 +66,7 @@ function onReady()
     
     update_genre_bg();
     
-    $('.overlay_container').click(backgroundClick);
+    //$('.overlay_container').click(backgroundClick);
 }
 
 function backgroundClick()
