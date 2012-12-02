beb46a069363a7f93e0ff2868789277fc3934d9a
Jim Lake <jim@blueskylabs.com>
2012-12-02 09:13:59 -0800

    unsupported browser

--- www/unsupported_browser.php
+++ www/unsupported_browser.php
@@ -36,46 +36,7 @@
             $return_url .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
         }
     }
+    
+    include_once "templates/unsupported_browser.html";
 
 ?>
-
-<html>
-<head>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
-
-<script type="text/javascript">
-
-var g_returnUrl = <?=json_encode($return_url);?>;
-
-function popChromeFrame()
-{
-    var opts = {
-        mode: "overlay"
-    };
-    
-    if( g_returnUrl )
-    {
-        opts['destination'] = g_returnUrl;
-    }
-
-    CFInstall.check(opts);
-}
-</script>
-
-</head>
-<body>
-<h3>Sorry!</h3>
-This site does not support your browser.<br/>
-<br/>
-<a href="http://firefox.com">Firefox</a><br/>
-<a href="http://www.google.com/chrome">Google Chrome</a><br/>
-<?php  if( $is_windows ):  ?>
-    <a href="http://windows.microsoft.com/en-US/internet-explorer/download-ie">Internet Explorer 9.0 or above</a><br/>
-<?php endif; ?>
-<?php  if( $is_msie ):  ?>
-    Or keep your current browser and enhance it with <a href="javascript:popChromeFrame();">Google Chrome Frame</a>
-<?php endif; ?>
-
-
-</body>
-
