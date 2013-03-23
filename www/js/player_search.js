1c324eff2e2d624b16ccac0f18fd88a97bba9449
Jim Lake <jim@blueskylabs.com>
2013-03-23 12:50:42 -0700

    scaled images

--- www/js/player_search.js
+++ www/js/player_search.js
@@ -82,10 +82,10 @@ function searchRun(s)
 {
     var re = new RegExp("(^|\\s)" + s,"i");
 
-    var artists = searchTestList(g_searchData.artists,re,true,"artist","/artists/files/");
-    var songs = searchTestList(g_searchData.songs,re,false,"song","/artists/files/");
-    var videos = searchTestList(g_searchData.videos,re,false,"video","/artists/files/");
-    var photos = searchTestList(g_searchData.photos,re,false,"photo","/artists/files/");
+    var artists = searchTestList(g_searchData.artists,re,true,"artist","");
+    var songs = searchTestList(g_searchData.songs,re,false,"song","");
+    var videos = searchTestList(g_searchData.videos,re,false,"video","");
+    var photos = searchTestList(g_searchData.photos,re,false,"photo","");
     
     var results = {
         'artists': artists,
