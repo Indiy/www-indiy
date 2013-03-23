a34cb041e4664b4ffb5140e611fc7da8e008d771
Jim Lake <jim@blueskylabs.com>
2013-03-23 10:16:19 -0700

    scaled images

--- www/manage/image_make_thumbnails.php
+++ www/manage/image_make_thumbnails.php
@@ -11,6 +11,9 @@
     header("Cache-Control: no-cache");
     header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
     
+    $ALT_IMAGE_REV_KEY = "1";
+    
+    
     error_reporting(E_ALL);
     
     session_start();
@@ -44,31 +47,41 @@
     
     use Aws\S3\S3Client;
     
-    function maybe_convert_and_upload_file($client,$src_image,$prefix,$width,$height,&$alts)
+    function maybe_convert_and_upload_file($client,$src_image,$prefix,$width,$height,&$extra)
     {
         $src_imagex = imagesx($src_image);
         $src_imagey = imagesy($src_image);
 
-        if( $src_imagex <= $width )
-        {
-            print " skip resize of image: $prefix ($src_imagex <= $width)";
-            return;
-        }
-        
-        $dst_key = "/test/{$prefix}_w{$width}";
+        $dst_imagex = $width;
         if( $height )
         {
-            $dst_key .= "_h{$height}";
+            $dst_imagey = $height;
         }
-        $dst_key .= ".jpg";
+        else
+        {
+            $dst_imagey = round($src_imagey / $src_imagex * $dst_imagex);
+        }
+
+        $alt_key = "w{$width}";
+        if( $height )
+        {
+            $alt_key .= "_h{$height}";
+        }
+        
+        $file_path = "/artists/thumbs/{$prefix}_{$alt_key}_{$ALT_IMAGE_REV_KEY}.jpg"
+
+
         
         try
         {
             $ret = $client->headObject(array(
                                              'Bucket' => 'static2.madd3v.com',
-                                             'Key' => $dst_key,
+                                             'Key' => $file_path,
                                              ));
-            print "  Image($dst_key) already exists, skipping\n";
+            print "  Image($file_path) already exists, skipping\n";
+            
+            $extra['alts']['alt_key'] = $file_path;
+            
             return;
         }
         catch( Exception $e )
@@ -101,14 +114,40 @@
         
         $args = array(
                       'Bucket' => 'static2.madd3v.com',
-                      'Key' => $dst_key,
+                      'Key' => $file_path,
                       'Body' => $img_data,
                       'ACL' => 'public-read',
                       'CacheControl' => 'public, max-age=22896000',
                       'ContentType' => 'image/jpeg',
                       );
         $client->putObject($args);
-        print " uploaded: $dst_key\n";
+        
+        $extra['alts']['alt_key'] = $file_path;
+
+        print " uploaded: $file_path\n";
+    }
+    
+    function needed_image_sizes($width)
+    {
+        $needed_widths = array(320,480,640,768,800,960,1024,
+                               1080,1280,1440,1536,1600,2048,
+                               400,500,600,700,900,1000,1100,1200,1300,
+                               1400,1500
+                               );
+        
+        $ret = array();
+        
+        foreach( $needed_widths => $w )
+        {
+            if( $w < $width )
+                $ret[] = array($w,FALSE);
+        }
+        
+        $ret[] = array(200,FALSE);
+        $ret[] = array(65,44);
+        $ret[] = array(210,132);
+        
+        return $ret;
     }
     
     try
@@ -129,7 +168,17 @@
         $q = mq($sql);
         while( $file = mf($q) )
         {
+            $id = $file['id'];
             $filename = $file['filename'];
+            $extra_json = $file['extra_json'];
+            if( $extra_json && strlen($extra_json) > 0 )
+            {
+                $extra = json_decode($extra_json,TRUE);
+            }
+            else
+            {
+                $extra = array();
+            }
             
             $url = artist_file_url($filename);
 
@@ -144,7 +193,28 @@
             
             $alts = array();
             
-            maybe_convert_and_upload_file($client,$src_image,$prefix,200,FALSE,$alts);
+            $width = imagesx($src_image);
+            $height = imagesy($src_image);
+            
+            if( !isset($extra['image_data']) )
+            {
+                $image_data = array("width" => $width,
+                                    "height" => $height,
+                                    );
+                $extra['image_data'] = $image_data;
+            }
+            
+            $needed_sizes = needed_image_sizes($width);
+            
+            foreach( $needed_sizes as $i => $size )
+            {
+                $f = maybe_convert_and_upload_file($client,$src_image,$prefix,$size[0],$size[1],$extra);
+            }
+            
+            $extra_json = json_encode($extra);
+            
+            mysql_update(array("extra_json" => $extra_json),'id',$id);
+            
             //maybe_convert_and_upload_file($client,$prefix,210,132,$alts);
             //maybe_convert_and_upload_file($client,$prefix,65,45,$alts);
             break;
