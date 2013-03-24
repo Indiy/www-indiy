58d9e86e61827ddda3752a1c4af8199cacc9fda7
Jim Lake <jim@blueskylabs.com>
2013-03-24 15:36:11 -0700

    more static hosting

---
+++ utils/publish_static.php
@@ -0,0 +1,90 @@
+<?php
+
+    $base_web_dir = $argv[1];
+    
+    if( !$base_web_dir || strlen($base_web_dir) == 0 )
+    {
+        print "Usage: php {$argv[0]} <base web dir>";
+        die();
+    }
+    
+    $base_web_dir = realpath($base_web_dir);
+    print "Base Dir: $base_web_dir\n";
+    
+    require_once "$base_web_dir/server_config.php";
+    require_once "$base_web_dir/includes/aws.phar";
+    
+    
+    $html_dir = $base_web_dir . "/html";
+
+    $file_map = array();
+    
+    function do_static_dir($html_dir,$web_path)
+    {
+        global $file_map;
+        global $client;
+        
+        $dir = "$html_dir$web_path";
+        
+        print "doing static dir: $dir\n";
+        
+        $dir_items = scandir($dir);
+        
+        foreach( $dir_items as $item )
+        {
+            $src_file = "$dir/$item";
+        
+            if( !is_file($src_file) )
+            {
+                print "  skip non-file: $item\n";
+                continue;
+            }
+            
+            $web_file = "$web_path/$item";
+
+            $hash = hash_file("md5",$src_file);
+            $key = "$web_path/{$hash}_$item";
+            print "  key: $key\n";
+            
+            try
+            {
+                $ret = $client->headObject(array(
+                                                 'Bucket' => $GLOBALS['g_aws_static_bucket'],
+                                                 'Key' => $key,
+                                                 ));
+                print "  $key already exists, skipping\n";
+            }
+            catch( Exception $e )
+            {
+                $args = array(
+                              'Bucket' => $GLOBALS['g_aws_static_bucket'],
+                              'Key' => $key,
+                              'SourceFile' => $src_file,
+                              'ACL' => 'public-read',
+                              'CacheControl' => 'public, max-age=300'
+                              );
+                $client->putObject($args);
+                print "  uploaded: $src_file\n";
+            }
+            $file_map[$web_file] = $key;
+        }
+        
+        print "done static dir\n";
+    }
+    
+    
+    $args = array(
+                  'key' => $GLOBALS['g_access_key_id'],
+                  'secret' => $GLOBALS['g_secret_access_key'],
+                  );
+    
+    $client = Aws\S3\S3Client::factory($args);
+    
+    do_static_dir($html_dir,"/css")
+
+    print "file_map: \n";
+    var_dump($file_map);
+    
+    print "\n\n";
+    print "done done\n";
+?>
