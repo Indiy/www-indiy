ad4dbb77690420f81697b657c86b51c99bb7ae70
Jim Lake <jim@blueskylabs.com>
2013-03-25 23:17:43 -0700

    updates for madcom

--- www/manage/image_make_thumbnails.php
+++ www/manage/image_make_thumbnails.php
@@ -162,41 +162,55 @@
                 $extra = array();
             }
             
-            $url = artist_file_url($filename);
-
-            $path_parts = pathinfo($filename);
-            $extension = $path_parts['extension'];
-            $prefix = str_replace(".$extension","",$filename);
+            $sql = "UPDATE artist_files SET processing = 1 WHERE id='$id' AND processing = 0";
+            $ret = mq($sql);
+            $row_count = mysql_affected_rows();
             
-            $src_data = file_get_contents($url);
-            $src_image = imagecreatefromstring($src_data);
-            
-            
-            $width = imagesx($src_image);
-            $height = imagesy($src_image);
-
-            print "filename: $filename ($width,$height)\n";
-            
-            if( !isset($extra['image_data']) )
+            if( $ret == TRUE && $row_count > 0 )
             {
-                $image_data = array("width" => $width,
-                                    "height" => $height,
-                                    );
-                $extra['image_data'] = $image_data;
+                $url = artist_file_url($filename);
+
+                $path_parts = pathinfo($filename);
+                $extension = $path_parts['extension'];
+                $prefix = str_replace(".$extension","",$filename);
+                
+                $src_data = file_get_contents($url);
+                $src_image = imagecreatefromstring($src_data);
+                
+                $width = imagesx($src_image);
+                $height = imagesy($src_image);
+
+                print "filename: $filename ($width,$height)\n";
+                
+                if( !isset($extra['image_data']) )
+                {
+                    $image_data = array("width" => $width,
+                                        "height" => $height,
+                                        );
+                    $extra['image_data'] = $image_data;
+                }
+                
+                $needed_sizes = needed_image_sizes($width);
+                
+                foreach( $needed_sizes as $i => $size )
+                {
+                    maybe_convert_and_upload_file($client,$src_image,$prefix,$size[0],$size[1],$extra);
+                }
+                
+                $extra_json = json_encode($extra);
+                
+                $updates = array(
+                                 "extra_json" => $extra_json,
+                                 "processing" => 0
+                                 );
+                mysql_update('artist_files',$updates,'id',$id);
+                print "  updated $id\n";
             }
-            
-            $needed_sizes = needed_image_sizes($width);
-            
-            foreach( $needed_sizes as $i => $size )
+            else
             {
-                $f = maybe_convert_and_upload_file($client,$src_image,$prefix,$size[0],$size[1],$extra);
+                print "  no update needed\n";
             }
-            
-            $extra_json = json_encode($extra);
-            
-            mysql_update('artist_files',array("extra_json" => $extra_json),'id',$id);
-            
-            print "updated $id\n";
+            print "\n";
             flush();
         }
     }
