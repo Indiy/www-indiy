f1b8f39ed0f9011487249a56d2f3df3600668dd9
Jim Lake <jim@blueskylabs.com>
2013-03-25 23:49:39 -0700

    updates for madcom

--- www/manage/image_make_thumbnails.php
+++ www/manage/image_make_thumbnails.php
@@ -38,6 +38,31 @@
     echo str_repeat(" ",1024);
     echo "<pre>\n";
     
+    
+    function image_needs_update($extra)
+    {
+        if( !isset($extra['image_data'])
+           || !isset($extra['image_data']['width'])
+           || !isset($extra['image_data']['height'])
+           )
+        {
+            print "  No valid image_data\n";
+            return TRUE;
+        }
+        if( !isset($extra['alts']) )
+        {
+            print "  No alts\n";
+            return TRUE;
+        }
+        if( count($extra['alts']) < 3 )
+        {
+            print "  Invalid alt set " . count($extra['alts']) . "\n";
+            return TRUE;
+        }
+        
+        return FALSE;
+    }
+    
     try
     {
         $client = get_s3_client();
@@ -58,53 +83,62 @@
                 $extra = array();
             }
             
-            $sql = "UPDATE artist_files SET processing = 1 WHERE id='$id' AND processing = 0";
-            $ret = mq($sql);
-            $row_count = mysql_affected_rows();
+            print "filename: $filename\n";
             
-            if( $ret == TRUE && $row_count > 0 )
+            if( image_needs_update($extra) )
             {
-                $url = artist_file_url($filename);
-
-                $path_parts = pathinfo($filename);
-                $extension = $path_parts['extension'];
-                $prefix = str_replace(".$extension","",$filename);
+                $sql = "UPDATE artist_files SET processing = 1 WHERE id='$id' AND processing = 0";
+                $ret = mq($sql);
+                $row_count = mysql_affected_rows();
                 
-                $src_data = file_get_contents($url);
-                $src_image = imagecreatefromstring($src_data);
-                
-                $width = imagesx($src_image);
-                $height = imagesy($src_image);
-
-                print "filename: $filename ($width,$height)\n";
-                
-                if( !isset($extra['image_data']) )
+                if( $ret == TRUE && $row_count > 0 )
                 {
-                    $image_data = array("width" => $width,
-                                        "height" => $height,
-                                        );
-                    $extra['image_data'] = $image_data;
+                    $url = artist_file_url($filename);
+
+                    $path_parts = pathinfo($filename);
+                    $extension = $path_parts['extension'];
+                    $prefix = str_replace(".$extension","",$filename);
+                    
+                    $src_data = file_get_contents($url);
+                    $src_image = imagecreatefromstring($src_data);
+                    
+                    $width = imagesx($src_image);
+                    $height = imagesy($src_image);
+
+                    print " size: ($width,$height)\n";
+                    
+                    if( !isset($extra['image_data']) )
+                    {
+                        $image_data = array("width" => $width,
+                                            "height" => $height,
+                                            );
+                        $extra['image_data'] = $image_data;
+                    }
+                    
+                    $needed_sizes = needed_image_sizes($width);
+                    
+                    foreach( $needed_sizes as $i => $size )
+                    {
+                        image_maybe_convert_and_upload_file($client,$src_image,$prefix,$size[0],$size[1],$extra);
+                    }
+                    
+                    $extra_json = json_encode($extra);
+                    
+                    $updates = array(
+                                     "extra_json" => $extra_json,
+                                     "processing" => 0
+                                     );
+                    mysql_update('artist_files',$updates,'id',$id);
+                    print "  updated $id\n";
                 }
-                
-                $needed_sizes = needed_image_sizes($width);
-                
-                foreach( $needed_sizes as $i => $size )
+                else
                 {
-                    image_maybe_convert_and_upload_file($client,$src_image,$prefix,$size[0],$size[1],$extra);
+                    print "  no update needed\n";
                 }
-                
-                $extra_json = json_encode($extra);
-                
-                $updates = array(
-                                 "extra_json" => $extra_json,
-                                 "processing" => 0
-                                 );
-                mysql_update('artist_files',$updates,'id',$id);
-                print "  updated $id\n";
             }
             else
             {
-                print "  no update needed\n";
+                print " no update needed!"
             }
             print "\n";
             flush();
