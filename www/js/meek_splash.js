7a17f933890763bba73600f42afa05b348bc8dfe
Jim Lake <jim@blueskylabs.com>
2013-03-29 14:50:13 -0700

    meek splash

--- www/js/meek_splash.js
+++ www/js/meek_splash.js
@@ -1,7 +1,10 @@
 
+
+var g_updateInterval = false;
+
 function splashReady()
 {
-    window.setInterval(updateCountdown,250);
+    g_updateInterval = window.setInterval(updateCountdown,250);
     updateCountdown();
 }
 $(document).ready(splashReady);
@@ -25,9 +28,18 @@ function updateCountdown()
     var hours = Math.floor( (secs_left / (60*60)) % 24 );
     var days = Math.floor( (secs_left / (24*60*60)) );
     
-    $('#top_container .top_line .days .time').html(sprintf("%02d",days));
-    $('#top_container .top_line .hours .time').html(sprintf("%02d",hours));
-    $('#top_container .top_line .minutes .time').html(sprintf("%02d",minutes));
-    $('#top_container .top_line .seconds .time').html(sprintf("%02d",seconds));
+    $('#top_container .top_line .days .time').html(getDigitHtml(days));
+    $('#top_container .top_line .hours .time').html(getDigitHtml(hours));
+    $('#top_container .top_line .minutes .time').html(getDigitHtml(minutes));
+    $('#top_container .top_line .seconds .time').html(getDigitHtml(seconds));
+}
+
+function getDigitHtml(value)
+{
+    var tens = Math.floor(value / 10);
+    var ones = value % 10;
+    
+    var html = "<div>{0}</div><div>{1}</div>".format(tens,ones);
+    return html;
 }
 
