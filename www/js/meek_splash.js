56a5cb5b8cb98a1bc9abd0f1592980654f04f628
Jim Lake <jim@blueskylabs.com>
2013-03-29 17:37:01 -0700

    meek splash

--- www/js/meek_splash.js
+++ www/js/meek_splash.js
@@ -1,5 +1,9 @@
 
 
+var EMAIL_REGEX = new RegExp('[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?');
+
+var PHONE_REGEX = new RegExp('^[0-9]{3}-[0-9]{3}-[0-9]{4}$')
+
 var g_updateInterval = false;
 
 function splashReady()
@@ -41,5 +45,73 @@ function getDigitHtml(value)
     
     var html = "<div>{0}</div><div>{1}</div>".format(tens,ones);
     return html;
+}
+
+function onKeyPressPhone(input,event)
+{
+    var val = input.value;
+    var key = Character.toChars(event.keyCode);
+    
+    if( val.length == 3 && key != '-' )
+    {
+        val += '-';
+    }
+    input.value = val;
+}
+
+function submitSplash()
+{
+    var name = $('#input_name').val();
+    var email = $('#input_email').val();
+    var phone = $('#input_phone').val();
+    
+    if( name.length == 0 || email.length == 0 || phone.length == 0 )
+    {
+        window.alert("Please fill out all form fields.");
+        return;
+    }
+    
+    if( !EMAIL_REGEX.match(email) )
+    {
+        window.alert("Please enter a valid email address.");
+        return;
+    }
+    
+    if( !PHONE_REGEX.match(phone) )
+    {
+        window.alert("Please enter a valid phone number.");
+        return;
+    }
+    
+    var args = {
+        name: name,
+        email: email,
+        phone: phone,
+        artist_id: g_artistId,
+        form_tag: g_formTag
+    };
+    
+    var url = "{0}/data/artist_form.php".format(g_apiBaseUrl);
+    
+    jQuery.ajax(
+    {
+        type: 'POST',
+        url: url,
+        data: args,
+        dataType: 'jsonp',
+        success: function(data) 
+        {
+            splashFormDone();
+        },
+        error: function()
+        {
+            window.alert("Form submission failed, please try again later.");
+        }
+    });
+}
+
+function splashFormDone()
+{
+    $('.form_hide').hide();
 }
 
