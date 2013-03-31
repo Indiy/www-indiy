a1f21905bffea26f3b205a088c9530732e26cf5e
Jim Lake <jim@blueskylabs.com>
2013-03-31 12:20:29 -0700

    aws push

--- www/manage/aws_publish.php
+++ www/manage/aws_publish.php
@@ -218,7 +218,7 @@
      
             $ret = $r53_client->listHostedZones($args);
             
-            foreach( $ret['HostedZones'] => $zone )
+            foreach( $ret['HostedZones'] as $zone )
             {
                 if( $zone['Name'] == $domain )
                     return $zone['Id'];
