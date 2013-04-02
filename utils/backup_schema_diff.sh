e54287192346af5e93f658cc5d0b5df247c1c16d
Jim Lake <jim@blueskylabs.com>
2013-04-01 20:09:35 -0700

    dbdiff for aws

--- utils/backup_schema_diff.sh
+++ utils/backup_schema_diff.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-SRC_DB=maddvcom_mysql
-DEST_DB=madcom_mysql
+SRC_DB=madd3v.com
+DEST_DB=madcom
+SRC_HOST=api.madd3v.com
+DEST_HOST=madprod.cghmds5s4dwn.us-east-1.rds.amazonaws.com
 DATE=$(date -u +"%Y_%m_%d_%H_%M_%S")
 BACKUP_FILE="madcom_backup_$DATE.sql"
 SQL_DIFF_FILE="madcom_sqldiff.sql"
 
-mysqldump --user=dbdiff --password=password $DEST_DB > $BACKUP_FILE
-mysqldiff --user=dbdiff --password=password $DEST_DB $SRC_DB > $SQL_DIFF_FILE
+PASSWORD=sad7823jba613kjbasuyeq
 
+mysqldump --user=dbdiff --password=$PASSWORD --host=$DEST_HOST $DEST_DB > $BACKUP_FILE
+mysqldiff --tolerant --user=dbdiff --password=$PASSWORD --host1=$DEST_HOST --host2=$SRC_HOST $DEST_DB $SRC_DB > $SQL_DIFF_FILE
--- utils/sync_db.sh
+++ utils/sync_db.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-SRC_DB=maddvcom_mysql
-DEST_DB=madcom_mysql
+SRC_DB=madd3v.com
+DEST_DB=madcom
+SRC_HOST=api.madd3v.com
+DEST_HOST=madprod.cghmds5s4dwn.us-east-1.rds.amazonaws.com
 DATE=$(date -u +"%Y_%m_%d_%H_%M_%S")
 BACKUP_FILE="madcom_backup_$DATE.sql"
 SQL_DIFF_FILE="madcom_sqldiff.sql"
 
-mysql --user=madcom_user --password="MyartistDNA!" $DEST_DB < $SQL_DIFF_FILE
+PASSWORD=sad7823jba613kjbasuyeq
+USER=dbdiff
+
+mysql --user=$USER --password=$PASSWORD --host=$DEST_HOST $DEST_DB < $SQL_DIFF_FILE
