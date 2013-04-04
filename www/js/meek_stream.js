84754f294d40470172068aab0ac02de3206831a1
Jim Lake <jim@blueskylabs.com>
2013-04-03 22:33:03 -0700

    meek stream

--- www/js/meek_stream.js
+++ www/js/meek_stream.js
@@ -4,7 +4,7 @@ function streamReady()
 {
     var height = $('#mad_tw_timeline').height();
     
-    var html = '<a class="twitter-timeline" height="{0}" href="https://twitter.com/search?q=%23meeklive" data-widget-id="319675836225699842">Tweets about "#meeklive"</a>'.format(height);
+    var html = '<a class="twitter-timeline" height="{0}" data-chrome="transparent" href="https://twitter.com/search?q=%23meeklive" data-widget-id="319675836225699842">Tweets about "#meeklive"</a>'.format(height);
     
     $('#mad_tw_timeline').html(html);
     //twttr.widgets.load();
