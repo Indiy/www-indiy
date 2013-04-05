c1a7be5ec1a31ece2e54a86d99b3995592f1d266
Jim Lake <jim@blueskylabs.com>
2013-04-04 21:54:23 -0700

    meek stream

--- www/js/meek_stream.js
+++ www/js/meek_stream.js
@@ -3,6 +3,14 @@ var IS_IPAD = navigator.userAgent.match(/iPad/i) != null;
 var IS_IPHONE = navigator.userAgent.match(/iPhone/i) != null;
 var IS_IPOD = navigator.userAgent.match(/iPod/i) != null;
 var IS_IOS = IS_IPAD || IS_IPHONE || IS_IPOD;
+
+var IS_ANDROID = navigator.userAgent.match(/Android/i) != null;
+var IS_ANDROID_PHONE = navigator.userAgent.match(/Android.*Mobile/i) != null;
+var IS_ANDROID_TABLET = IS_ANDROID && !IS_ANDROID_PHONE;
+
+var IS_PHONE = IS_IPOD || IS_IPHONE || IS_ANDROID_PHONE;
+var IS_TABLET = IS_ANDROID_TABLET || IS_IPAD;
+
 var IS_WINDOWS = navigator.userAgent.match(/Windows/i) != null;
 var IS_CHROME = navigator.userAgent.match(/Chrome/i) != null;
 var IS_MOBILE = navigator.userAgent.match(/Mobile/i) != null;
@@ -53,7 +61,11 @@ function twitterWidgetLoad()
     if( typeof twttr != 'undefined' )
     {
         twttr.widgets.load();
-        showTwitter();
+        
+        if( !IS_PHONE )
+        {
+            showTwitter();
+        }
     }
     else
     {
