1858529adae807c81ea4525d08fa1fd14966afc9
Jim Lake <jim@blueskylabs.com>
2013-04-18 10:49:33 -0700

    add video

--- www/manage/data/video.php
+++ www/manage/data/video.php
@@ -66,36 +66,18 @@
         $artist_id = $_POST["artistid"];
         $video_id = $_POST["id"];
         
-        $upload_video_filename = NULL;
-        if( $video_id != "" ) 
-        {
-            $row = mf(mq("SELECT `id`,`image`,`video` FROM mydna_musicplayer_video WHERE `id`='$video_id'"));
-            $old_image_file = $row["image"];
-            $old_video_file = $row["video"];
-            $upload_video_filename = $row["upload_video_filename"];
-            $old_image_data = $row['image_data'];
-        }
-        
         $video_name = my($_POST["name"]);
         $video_tags = $_POST["tags"];
-        $error = NULL;
-        
-        if( $_POST["remove_video_image"] == 'true' )
-            $old_logo = '';
-        if( $_POST["remove_video"] == 'true' )
-            $old_sound = '';
-            
         
         $image_file = $_POST['image_drop'];
         $video_file = $_POST['video_drop'];
         
-        $values = array("artistid" => $artist_id,
+        $values = array(
+                        "artistid" => $artist_id,
                         "name" => $video_name,
                         "image" => $image_file,
                         "video" => $video_file,
-                        "upload_video_filename" => $upload_video_filename,
                         "tags" => $tags,
-                        "error" => $error,
                         );
         
         if( $video_id != "") 
@@ -108,6 +90,7 @@
             $video_id = mysql_insert_id();
         }
         
+        $postedValues['video_id'] = $video_id;
         $postedValues['imageSource'] = $video_logo;
         $postedValues['video_sound'] = $video_file;
         $postedValues['success'] = "1";
