bf8143a3d4578b910c8ffdc72b11920e9c3856d3
Alphonso Morris <amorris@levasent.com>
2013-05-28 10:36:16 -0400

    home img edit

--- www/js/home_data.js
+++ www/js/home_data.js
@@ -22,7 +22,7 @@ var g_backgroundList = [
     },
     
     {	
-    	image: "/images/home_bg03.jpg",
+    	image: "/images/home_bg04.jpg",
         bg_color: "000000",
         bg_style: "STRETCH",
         bg_justify: "TOP",
@@ -32,9 +32,21 @@ var g_backgroundList = [
             "<div class='logo'></div><a href='http://denmbar.myartistdna.com' target='_blank'>DEN.M BAR<div class='slashes'>//</div>CLOTHING LINE</a>"      
        
     },
-    
+
+    {	
+    	image: "/images/home_bg05.jpg",
+        bg_color: "000000",
+        bg_style: "STRETCH",
+        bg_justify: "TOP",
+        loaded: false,
+        image_data: { width:1267, height:800 },
+        content_info_html:
+            "<div class='logo'></div><a href='http://denmbar.myartistdna.com' target='_blank'>DEN.M BAR<div class='slashes'>//</div>CLOTHING LINE</a>"      
+       
+    },
+
     {
-        image: "/images/home_bg04.jpg",
+        image: "/images/home_bg03.jpg",
         bg_color: "000000",
         bg_style: "STRETCH",
         bg_justify: "TOP",
