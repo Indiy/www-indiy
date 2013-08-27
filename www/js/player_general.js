bb6abf5f8a5302ea839f12a4d9ce6c6bd6675007
Jim Lake <jim@blueskylabs.com>
2013-08-26 22:24:12 -0700

    default v2

--- www/js/player_general.js
+++ www/js/player_general.js
@@ -9,6 +9,16 @@ var IS_IPOD = navigator.userAgent.match(/iPod/i) != null;
 var IS_IOS = IS_IPAD || IS_IPHONE || IS_IPOD;
 
 var IS_ANDROID = navigator.userAgent.match(/Android/i) != null;
+var IS_ANDROID_PHONE = navigator.userAgent.match(/Android.*Mobile/i) != null;
+var IS_ANDROID_TABLET = IS_ANDROID && !IS_ANDROID_PHONE;
+
+var IS_PHONE = IS_IPOD || IS_IPHONE || IS_ANDROID_PHONE;
+var IS_TABLET = IS_ANDROID_TABLET || IS_IPAD;
+
+var IS_WINDOWS = navigator.userAgent.match(/Windows/i) != null;
+var IS_CHROME = navigator.userAgent.match(/Chrome/i) != null;
+var IS_MOBILE = navigator.userAgent.match(/Mobile/i) != null;
+var IS_DESKTOP = !IS_MOBILE;
 
 var IS_IE = false;
 var IS_OLD_IE = false;
