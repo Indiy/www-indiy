11c13d7de25c8b0238deebd97ad392fab80c0f8c
Jim Lake <jim@blueskylabs.com>
2013-08-31 22:43:15 -0700

    playlists

--- www/manage/data/playlist_items.php
+++ www/manage/data/playlist_items.php
@@ -64,19 +64,18 @@ function get_playlist_item($playlist_item_id)
     $sql .= " LEFT JOIN artist_files ON playlist_items.image_id = artist_files.id ";
     $sql .= " WHERE playlist_item_id = '$playlist_item_id' ";
     $q = mq($sql);
-    while( $row = mf($q) )
+    $row = mf($q);
+    $image_extra = json_decode($row['image_extra_json'],TRUE);
+    $row['image_extra'] = $image_extra;
+    if( !empty($row['image']) )
     {
-        $image_extra = json_decode($row['image_extra_json'],TRUE);
-        $row['image_extra'] = $image_extra;
-        if( !empty($row['image']) )
-        {
-            $row['image_url'] = artist_file_url($row['image']);
-        }
-        else
-        {
-            $row['image_url'] = "images/photo_video_01.jpg";
-        }
+        $row['image_url'] = artist_file_url($row['image']);
     }
+    else
+    {
+        $row['image_url'] = "images/photo_video_01.jpg";
+    }
+    return $row;
 }
 
 function do_POST()
