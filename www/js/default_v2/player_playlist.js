fbb680dc6376eeb5b9ef4fc7fdc370654245496a
Jim Lake <jim@blueskylabs.com>
2013-09-08 13:12:39 -0700

    v3 playlist

--- www/js/default_v2/player_playlist.js
+++ www/js/default_v2/player_playlist.js
@@ -21,7 +21,7 @@ function clickPlaylist(index)
             var image_url = getImgUrlWithWidth(pi,233);
             
             var html = "";
-            html += "<div id='item_{1}' class='item inactive' onclick='clickPlaylistItem({0},{1});'>".format(index,i);
+            html += "<div id='item_{1}' class='item inactive' onclick='clickPlaylistDirItem({0},{1});'>".format(index,i);
             html += " <img src='{0}'/>".format(image_url);
             html += " <div class='overlay'></div>";
             html += " <div class='name'>{0}</div>".format(pi.name);
@@ -32,3 +32,31 @@ function clickPlaylist(index)
     }
 }
 
+function clickPlaylistDirItem(playlist_index,child_playlist_index)
+{
+    var playlist = g_playlistList[index];
+    var child_playlist = playlist.items[child_playlist_index];
+    
+    $('#playlist_tab .item_column.dir').children().removeClass('active');
+    $('#playlist_tab .item_column.dir').children().addClass('inactive');
+
+    var sel = "#playlist_tab .item_column.dir #item_{0}".format(index);
+    $(sel).removeClass('inactive');
+    $(sel).addClass('active');
+    
+    $('#playlist_tab .item_column.media').empty();
+    for( var i = 0 ; i < child_playlist.items.length ; ++i )
+    {
+        var pi = child_playlist.items[i];
+        var image_url = getImgUrlWithWidth(pi,233);
+        
+        var html = "";
+        html += "<div id='item_{1}' class='item inactive' onclick='clickPlaylistMediaItem({0},{1});'>".format(index,i);
+        html += " <img src='{0}'/>".format(image_url);
+        html += " <div class='overlay'></div>";
+        html += " <div class='name'>{0}</div>".format(pi.name);
+        html += "</div>";
+        
+        $('#playlist_tab .item_column.media').append(html);
+    }
+}
