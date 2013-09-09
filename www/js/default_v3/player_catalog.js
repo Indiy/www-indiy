bb8447d3253a1f15a223f39adb9fd84cdf18fbc5
Jim Lake <jim@blueskylabs.com>
2013-09-08 21:56:05 -0700

    v3 playlist

---
+++ www/js/default_v3/player_catalog.js
@@ -0,0 +1,95 @@
+
+function catalogClickPlaylist(index)
+{
+    var playlist = g_playlistList[index];
+    
+    $('#playlist_tab .item_column.playlist').children().removeClass('active');
+    $('#playlist_tab .item_column.playlist').children().addClass('inactive');
+
+    var sel = "#playlist_tab .item_column.playlist #item_{0}".format(index);
+    $(sel).removeClass('inactive');
+    $(sel).addClass('active');
+    
+    
+    $('#playlist_tab .item_column.dir').empty();
+    $('#playlist_tab .item_column.media').empty();
+    
+    if( playlist.type == 'DIR' )
+    {
+        $('#playlist_tab .headers .column2').show();
+        $('#playlist_tab .item_column.dir').show();
+        for( var i = 0 ; i < playlist.items.length ; ++i )
+        {
+            var pi = playlist.items[i];
+            var image_url = getImgUrlWithWidth(pi,233);
+            
+            var html = "";
+            html += "<div id='item_{1}' class='item inactive' onclick='catalogClickPlaylistDirItem({0},{1});'>".format(index,i);
+            html += " <img src='{0}'/>".format(image_url);
+            html += " <div class='overlay'></div>";
+            html += " <div class='name'>{0}</div>".format(pi.name);
+            html += "</div>";
+            
+            $('#playlist_tab .item_column.dir').append(html);
+        }
+    }
+    else
+    {
+        $('#playlist_tab .headers .column2').hide();
+        $('#playlist_tab .item_column.dir').hide();
+        var playlist_tuple = "{0}".format(index);
+        populateMedia(playlist,playlist_tuple);
+    }
+}
+
+function catalogClickPlaylistDirItem(playlist_index,child_playlist_index)
+{
+    var playlist = g_playlistList[playlist_index];
+    var child_playlist = playlist.items[child_playlist_index];
+    
+    $('#playlist_tab .item_column.dir').children().removeClass('active');
+    $('#playlist_tab .item_column.dir').children().addClass('inactive');
+
+    var sel = "#playlist_tab .item_column.dir #item_{0}".format(child_playlist_index);
+    $(sel).removeClass('inactive');
+    $(sel).addClass('active');
+    
+    var playlist_tuple = "{0},{1}".format(playlist_index,child_playlist_index);
+    populateMedia(child_playlist,playlist_tuple);
+}
+function populateMedia(playlist,playlist_tup)
+{
+    $('#playlist_tab .item_column.media').empty();
+    for( var i = 0 ; i < playlist.items.length ; ++i )
+    {
+        var pi = playlist.items[i];
+        var image_url = getImgUrlWithWidth(pi,233);
+        
+        var media_tup = "{0},{1}".format(playlist_tup,i);
+        
+        var html = "";
+        html += "<div id='item_{1}' class='item inactive' onclick='catalogClickPlaylistMediaItem({0});'>".format(media_tup,i);
+        html += " <img src='{0}'/>".format(image_url);
+        html += " <div class='overlay'></div>";
+        html += " <div class='name'>{0}</div>".format(pi.name);
+        html += "</div>";
+        
+        $('#playlist_tab .item_column.media').append(html);
+    }
+}
+
+function catalogClickPlaylistMediaItem(playlist_index,child_playlist_index,playlist_item_index)
+{
+    hideAllTabs();
+    var playlist = g_playlistList[playlist_index];
+    
+    if( typeof playlist_item_index !== 'undefined' )
+    {
+        playlist = playlist.items[child_playlist_index];
+    }
+    else
+    {
+        playlist_item_index = child_playlist_index;
+    }
+    playlistChangePlaylist(playlist,playlist_item_index);
+}
--- www/js/default_v3/player_playlist.js
+++ www/js/default_v3/player_playlist.js
@@ -4,6 +4,8 @@ var g_musicIsPlaying = false;
 var g_videoPlayerReady = false;
 var g_musicPlayerReady = false;
 var g_videoContainers = 0;
+var g_currentPlaylist = false;
+var g_currentPlaylistIndex = 0;
 
 function playlistReady()
 {
@@ -87,87 +89,8 @@ function makeCallback(callback,arg1)
     return f;
 }
 
-function clickPlaylist(index)
+function playlistChangePlaylist(new_playlist,playlist_item_index)
 {
-    var playlist = g_playlistList[index];
-    
-    $('#playlist_tab .item_column.playlist').children().removeClass('active');
-    $('#playlist_tab .item_column.playlist').children().addClass('inactive');
-
-    var sel = "#playlist_tab .item_column.playlist #item_{0}".format(index);
-    $(sel).removeClass('inactive');
-    $(sel).addClass('active');
-    
-    if( playlist.type == 'DIR' )
-    {
-        $('#playlist_tab .item_column.dir').empty();
-        $('#playlist_tab .item_column.media').empty();
-        
-        for( var i = 0 ; i < playlist.items.length ; ++i )
-        {
-            var pi = playlist.items[i];
-            var image_url = getImgUrlWithWidth(pi,233);
-            
-            var html = "";
-            html += "<div id='item_{1}' class='item inactive' onclick='clickPlaylistDirItem({0},{1});'>".format(index,i);
-            html += " <img src='{0}'/>".format(image_url);
-            html += " <div class='overlay'></div>";
-            html += " <div class='name'>{0}</div>".format(pi.name);
-            html += "</div>";
-            
-            $('#playlist_tab .item_column.dir').append(html);
-        }
-    }
-}
-
-function clickPlaylistDirItem(playlist_index,child_playlist_index)
-{
-    var playlist = g_playlistList[playlist_index];
-    var child_playlist = playlist.items[child_playlist_index];
-    
-    $('#playlist_tab .item_column.dir').children().removeClass('active');
-    $('#playlist_tab .item_column.dir').children().addClass('inactive');
-
-    var sel = "#playlist_tab .item_column.dir #item_{0}".format(child_playlist_index);
-    $(sel).removeClass('inactive');
-    $(sel).addClass('active');
-    
-    $('#playlist_tab .item_column.media').empty();
-    for( var i = 0 ; i < child_playlist.items.length ; ++i )
-    {
-        var pi = child_playlist.items[i];
-        var image_url = getImgUrlWithWidth(pi,233);
-        
-        var tup = "{0},{1},{2}".format(playlist_index,child_playlist_index,i);
-        
-        var html = "";
-        html += "<div id='item_{1}' class='item inactive' onclick='clickPlaylistMediaItem({0});'>".format(tup,i);
-        html += " <img src='{0}'/>".format(image_url);
-        html += " <div class='overlay'></div>";
-        html += " <div class='name'>{0}</div>".format(pi.name);
-        html += "</div>";
-        
-        $('#playlist_tab .item_column.media').append(html);
-    }
-}
-
-var g_currentPlaylist = false;
-var g_currentPlaylistIndex = 0;
-
-function clickPlaylistMediaItem(playlist_index,child_playlist_index,playlist_item_index)
-{
-    hideAllTabs();
-    var playlist = g_playlistList[playlist_index];
-    
-    if( typeof playlist_item_index !== 'undefined' )
-    {
-        playlist = playlist.items[child_playlist_index];
-    }
-    else
-    {
-        playlist_item_index = child_playlist_index;
-    }
-    
     var old_playlist = g_currentPlaylist;
     if( old_playlist.video_player )
     {
@@ -175,10 +98,9 @@ function clickPlaylistMediaItem(playlist_index,child_playlist_index,playlist_ite
         $(old_playlist.video_container_sel).hide();
     }
     
-    g_currentPlaylist = playlist;
-    
+    g_currentPlaylist = new_playlist;
     $('.full_screen_bg').hide();
-    $(playlist.bg_sel).show();
+    $(new_playlist.bg_sel).show();
 
     currentPlaylistChangeIndex(playlist_item_index);
 }
