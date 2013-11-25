4caba59b7a196f5a6dfd717c889920c048d63eb4
Jim Lake <jim@blueskylabs.com>
2013-11-24 20:30:48 -0800

    fix alignment

--- www/js/default_v3/player_playlist.js
+++ www/js/default_v3/player_playlist.js
@@ -235,7 +235,10 @@ function swipePanelChange(that,playlist,index)
     genericUpdateViews('media',playlist_item.playlist_item_id,playlist_item);
     playlistSetVolume(g_volRatio);
     
-    $(playlist.bg_sel).swipe('fixupscroll');
+    window.setTimeout(function()
+    {
+        $(playlist.bg_sel).swipe('fixupscroll');
+    },100);
 }
 function playlistLoadImage(playlist,index)
 {
