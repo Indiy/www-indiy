ebf560a957f68131e2f3423325b9a4ba927efd1e
Jim Lake <jim@blueskylabs.com>
2014-04-13 15:57:08 -0700

    fullscreen

--- www/js/default_v3/default_v3.js
+++ www/js/default_v3/default_v3.js
@@ -244,3 +244,48 @@ function showInstagram()
     $('#social_box .social_item').hide();
     $('#social_box .social_instagram').show();
 }
+
+function toggleFullscreen()
+{
+    if( !document.fullscreenElement
+        && !document.mozFullScreenElement
+        && !document.webkitFullscreenElement
+        && !document.msFullscreenElement )
+    {
+        if( document.documentElement.requestFullscreen )
+        {
+            document.documentElement.requestFullscreen();
+        }
+        else if( document.documentElement.msRequestFullscreen )
+        {
+            document.documentElement.msRequestFullscreen();
+        }
+        else if( document.documentElement.mozRequestFullScreen )
+        {
+            document.documentElement.mozRequestFullScreen();
+        }
+        else if( document.documentElement.webkitRequestFullscreen )
+        {
+            document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    }
+    else
+    {
+        if( document.exitFullscreen )
+        {
+            document.exitFullscreen();
+        }
+        else if( document.msExitFullscreen )
+        {
+            document.msExitFullscreen();
+        }
+        else if( document.mozCancelFullScreen )
+        {
+            document.mozCancelFullScreen();
+        }
+        else if( document.webkitExitFullscreen )
+        {
+            document.webkitExitFullscreen();
+        }
+    }
+}
