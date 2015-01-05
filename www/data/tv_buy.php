00cd7ab3a77a116fd42e662c3498f72a72c6922d
Jim Lake <jim@blueskylabs.com>
2015-01-04 21:45:14 -0800

    stripe

---
+++ www/data/tv_buy.php
@@ -0,0 +1,32 @@
+<?php
+
+    header("Content-Type: application/json");
+    header("Cache-Control: no-cache, no-store, must-revalidate");
+    header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
+    header("Access-Control-Allow-Origin: *");
+
+    require_once '../includes/functions.php';
+    require_once '../includes/config.php';
+
+    require_once 'Stripe.php';
+
+    Stripe::setApiKey($stripe['secret_key']);
+
+    $token = $_REQUEST['token'];
+    $email = $_REQUEST['email'];
+
+    $customer = Stripe_Customer::create(array(
+        'email' => $email,
+        'card'  => $token,
+    ));
+
+    $charge = Stripe_Charge::create(array(
+        'customer' => $customer->id,
+        'amount'   => 5000,
+        'currency' => 'usd',
+    ));
+
+    echo $charge;
+
+    die();
+?>
